a461a59cfb4b9c42292e69746226cf73d1c4e61b
Amy Pitts <amy.pitts1@marist.edu>
2018-06-13 14:17:41 -0400

    Adding to the strucchange file

--- REU_Initial_6_12_18.R
+++ REU_Initial_6_12_18.R
@@ -16,15 +16,20 @@ library("strucchange")
 ?breakpoints
 
 #Bai-Perron Method
-dif_means_ts_0<-ts(first, start=1, end=30) #create time series data on only the first data 
-breakpoints(dif_means_ts_0 ~ 1, breaks = 2) #no break points 
+dif_means_ts_0<-ts(first, start=1,end=30) #create time series data on only the first data 
+breakpoints(dif_means_ts_0 ~ 1, breaks = 2 ) #no break points 
+plot(dif_means_ts_0)
 
 dif_means_ts_1<-ts(dif_means, start=1, end=60) #create time series data with first and second data
-breakpoints(dif_means_ts_1 ~ 1, breaks = 2) #one break
+breakpoints(dif_means_ts_1 ~ 1, breaks = 2 , h=0.1) #one break
+plot(dif_means_ts_1)
+points(30, dif_means_ts_1[30], col="purple", pch= 18)
 
 dif_means_ts_2<-ts(dif_means_2, start=1, end=90) #create time series data with first second and third data
-breakpoints(dif_means_ts_2 ~ 1, breaks=3) #two breaks 
-#hi peeps
+breakpoints(dif_means_ts_2 ~ 1, breaks=3, h=0.1) #two breaks 
+plot(dif_means_ts_2)
+points(30, dif_means_ts_2[30], col="purple", pch= 18)
+points(60, dif_means_ts_2[60], col="purple", pch= 18)
 
 
 
