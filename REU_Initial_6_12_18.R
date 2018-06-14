360623ea73d6cfd30376cf7bd91f7aaa219f7950
sneitzel <sarahbneitzel.wa@gmail.com>
2018-06-14 12:06:28 -0400

    Evaluation w/ SSE in initial file.

--- REU_Initial_6_12_18.R
+++ REU_Initial_6_12_18.R
@@ -36,28 +36,31 @@ bkpts_2$breakpoints #list of x-values (time) for breakpoints
 
 #-----------------------------------------------------------------
 
+k <- c(1,30,60)
+
+#install.packages("apricom")
+library("apricom") #if this is not working install the package above
+
 #sd and sse
 sum_sd = 0
-sum_sse = 0
-#install.packages("apricom")
-library("apricom") #if this is not working install the package above 
-for(i in k) {
-  min = i-1 
-  if(!(min == 0)){
-    x_values = test_data[c(min:i),1] #getting the x values in the interval
-    y_values = test_data[c(min:i),2] #getting the y values in the interval
-    data = data.frame(x_values, y_values) #re-makning this into a dataframe 
-    sum_sd = sum_sd + sd(y_values) #adding up all the standard deviations
-    lm.fit = lm(y_values ~ x_values) #linear fit 
-    sum_sse = sum_sse + sse(lm.fit$coefficients, y_values) #finding the SSE 
+sum_SSE = 0
+for(i in 1:length(k)) {
+  if(k[i] != 1){
+	min = k[i-1]
+	x_values = test_data[c(min:i),1] #getting the x values in the interval
+	y_values = test_data[c(min:i),2] #getting the y values in the interval
+	data = data.frame(x_values, y_values) #re-making this into a dataframe 
+	sum_sd = sum_sd + sd(y_values) #adding up all the standard deviations
+	model = lm(y_values~x_values)
+	SSE = sum(model$residuals**2)
+	sum_SSE = sum_sse + SSE #finding the SSE 
   }
 }
 sigma_new = sum_sd / length(k)-1   
-SSE_new = sum_sse / length(k)-1     
-
+SSE_new = sum_sse / length(k)-1 
 
 sigma_old = sd()      #need this 
-SEE_old =             #sse(b, dataset)
+SEE_old =
 
 ratio = exp(-1/(2 * sigma_new) * SSE_new) + exp(-1/(2 * sigma_old) * SEE_old)
 u = runif(1) #random number from 0 to 1 taken from a normal distrabution 
--- addition_attempt.R
+++ addition_attempt.R
@@ -7,7 +7,7 @@ dif_means_ts_1<-ts(dif_means, start=1, end=60) #create time series data with fir
 bkpts_1<-breakpoints(dif_means_ts_1 ~ 1, breaks = 2 , h=0.1) #one break
 bkpts_1_ends<-c(1, bkpts_1$breakpoints, length(dif_means_ts_1)) #breakpoint set with end points
 
-barBirth2<-function(currentSet){
+barMake2<-function(currentSet){
 
 k_ends = currentSet #current set of breakpoints with ends
 d = diff(k_ends) #finding the distance between all those breakpoints
@@ -21,8 +21,8 @@ newSet
 
 }
 
-barBirth2(bkpts_1_ends)
-barBirth2(c(1,50,60))
+barMake2(bkpts_1_ends)
+barMake2(c(1,50,60))
 
 plot(test_data) #plotting the original data 
 points(new_addition, dif_means[new_addition], col="red", pch= 17) #plotting the new break point
