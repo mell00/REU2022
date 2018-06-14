5b76e45eeeeab797d15a68141ae91782415c79f6
Amy Pitts <amy.pitts1@marist.edu>
2018-06-14 10:42:36 -0400

    Changing styling

--- addition_attempt.R
+++ addition_attempt.R
@@ -9,15 +9,15 @@ bkpts_1_ends<-c(1, bkpts_1$breakpoints, length(dif_means_ts_1)) #breakpoint set 
 
 barBirth2<-function(currentSet){
 
-k_ends = currentSet #current set of breakpoints with ends
-d = diff(k_ends) #finding the distance between all those breakpoints
-locations = which(d == max(d) ) #finding the location(s) of the farthest distance 
-location = sample(locations, 1) #randomly select location in case of equal max distances
-min = k_ends[location] #lower bound 
-max = k_ends[location + 1] #upper bound
-new_addition = sample((min+1):(max-1), 1) #selecting a random number in the correct interval
-newSet = sort(c(k_ends, new_addition))
-newSet
+  k_ends = currentSet #current set of breakpoints with ends
+  d = diff(k_ends) #finding the distance between all those breakpoints
+  locations = which(d == max(d) ) #finding the location(s) of the farthest distance 
+  location = sample(locations, 1) #randomly select location in case of equal max distances
+  min = k_ends[location] #lower bound 
+  max = k_ends[location + 1] #upper bound
+  new_addition = sample((min+1):(max-1), 1) #selecting a random number in the correct interval
+  newSet = sort(c(k_ends, new_addition))
+  newSet
 
 }
 
