79a7b763d459075004715dd4265df7cdde30ef14
sneitzel <sarahbneitzel.wa@gmail.com>
2018-06-14 13:51:39 -0400

    Streamlined addition attempts 1 and 2.

--- addition_attempt_two.R
+++ addition_attempt_two.R
@@ -1,19 +1,11 @@
-#subtraction_attempt 
+#addition attempt two (proposes two new breakpoints and scores - bigger min, bigger max, then random)
 
-#setting up data 
-first = rnorm(30, mean = 5, sd = 1) #30 random data points from a normal distribution with means 5
-second = rnorm(30, mean = 15, sd = 1)
-dif_means = c(first, second) #adding the two sets of data points together 
+k_ends_test = c(1,50,60)
 
-dif_means_ts_1<-ts(dif_means, start=1, end=60) #create time series data with first and second data
-time = c(1:60) #making time 
-test_data = data.frame(time, dif_means) #our data with x = time and y = two sets of data with two means 
-
-bkpts_1<-breakpoints(dif_means_ts_1 ~ 1, breaks = 2 , h=0.1) #one break
-bkpts_1_ends<-c(1, bkpts_1$breakpoints, length(dif_means_ts_1)) #breakpoint set with end points
+barMake2<-function(k_ends){
 
 #random number generator 
-proposed = sample(1:length(test_data[,1]), 2)
+proposed = sample(1:max(k_ends), 2)
 proposed = sort(proposed) #sorting the 2 chosen numbers 
 
 #setting up initial values 
@@ -23,20 +15,20 @@ small_2 = list()
 big_2 = list()
 
 #finding which points are closest to the propsed break points 
-for(i in 1:length(k)) {
-  if(proposed[1] > k[i] ) {
-    small_1 = c(small_1, k[i])
+for(i in 1:length(k_ends)) {
+  if(proposed[1] > k_ends[i] ) {
+    small_1 = c(small_1, k_ends[i])
   } else {
-    big_1 = c(big_1, k[i])
+    big_1 = c(big_1, k_ends[i])
   }
-  if(proposed[2] > k[i]) {
-    small_2 = c(small_2, k[i])
+  if(proposed[2] > k_ends[i]) {
+    small_2 = c(small_2, k_ends[i])
   } else {
-    big_2 = c(big_2, k[i])
+    big_2 = c(big_2, k_ends[i])
   }
 }
 
-#this is extremely conveluted and not the best way 
+#this is extremely convaluted and not the best way 
 one = c(small_1[which.max(small_1)][[1]][1], proposed[[1]][1], big_1[which.min(big_1)][[1]][1]) 
 two = c(small_2[which.max(small_2)][[1]][1], proposed[[2]][1], big_2[which.min(big_2)][[1]][1])
 
@@ -67,4 +59,8 @@ if(d_one_sort[1] == d_two_sort[1]) { #comparing the mins, if equal, look at max
 } else {
   new_bp = proposed[1]
 }
-new_bp #the breakpoint 
+  newSet = sort(c(k_ends, new_bp))
+  newSet
+}
+
+barMake2(k_ends_test)
