d4780ddbbc8320f26682bbe8a1a959ed82c51be1
haglichk <khaglich@gmail.com>
2018-06-15 10:27:14 -0400

    commenting in subtraction

--- subtraction_attempt_random.R
+++ subtraction_attempt_random.R
@@ -1,14 +1,14 @@
 #subtraction attempt zero - random murder
 
-k_ends_test = c(1,50,55,60)
+k_ends_test = c(1,50,55,60) #test k thing 
 
 barMurder0<-function(k_ends){
 
-	k = k_ends_test[c(-1,-length(k_ends_test))]
-	random_num = sample(1:length(k), 1)
-	k_ends_final = k_ends[-(random_num+1)]
-	k_ends_final
+	k = k_ends_test[c(-1,-length(k_ends))] #removing first and last value 
+	random_num = sample(1:length(k), 1) #chooses random number 
+	k_ends_final = k_ends[-(random_num+1)] #removes random number 
+	k_ends_final #returns it 
 
 }
 
-barMurder0(k_ends_test)
+barMurder0(k_ends_test) #calls test 
