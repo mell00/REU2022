71a88fad092d0c09df35058844d6a370416da11b
Amy Pitts <amy.pitts1@marist.edu>
2018-06-19 09:59:41 -0400

    Adding the gif and changing graphing

--- trying_graphing.R
+++ trying_graphing.R
@@ -1,22 +1,39 @@
-#example of bad splines 
-neuron = read.table("exampledata.txt")
+#creating our data 
+first = rnorm(30, mean = 5, sd = 1) #30 random data points from a normal distribution with mean of 5
+second = rnorm(30, mean = 15, sd = 1) #30 random data points from a normal distribution with mean of 15
+third = rnorm(30, mean = 30, sd = 1) #30 random data points from a normal distribution with mean of 30
 
-new_times = c(1:110)
+dif_means_0 = c(first) #setting up null set
+dif_means_1 = c(first, second) #adding two sets of data points together 
+dif_means_2 = c(first, second, third) #adding three sets of data points together
 
-neuron_time = data.frame(new_times, as.numeric(neuron$V2))
+time_0 = 1:30
+time_1 = 1:60
+time_2 = 1:90
 
+test_data_0 = data.frame(time_0, dif_means_0) #our data with x = time and y = data from 1 set
+test_data_1 = data.frame(time_1, dif_means_1) #our data with x = time and y = data from 2 sets
+test_data_2 = data.frame(time_2, dif_means_2) #our data with x = time and y = data from 3 sets
+
+#downloading the strucchange package
+#install.packages("strucchange")
 library("strucchange")
-dif_means_neuron<-ts(neuron_time[,2], start=1, end=110) #create time series data on only the first data 
-bkpts_neuron<-breakpoints(dif_means_neuron ~ 1, breaks = 5, h=.05) #no break points 
-plot(dif_means_neuron)
-points(43,neuron[43,2], col="purple", pch= 18)
-points(48,neuron[48,2], col="purple", pch= 18)
-points(56,neuron[56,2], col="purple", pch= 18)
-points(72,neuron[72,2], col="purple", pch= 18)
-points(98,neuron[98,2], col="purple", pch= 18)
+
+#Bai-Perron Method
+bkpts_0 = breakpoints(test_data_0$dif_means_0 ~ test_data_0$time_0, breaks = 5, h = 0.1) #no break points 
+bkpts_0$breakpoints #list of x-values (time) for breakpoints
+
+bkpts_1 = breakpoints(test_data_1$dif_means_1 ~ test_data_1$time_1, breaks = 5, h = 0.1) #no break points 
+bkpts_1$breakpoints #list of x-values (time) for breakpoints
+
+bkpts_2 = breakpoints(test_data_2$dif_means_2 ~ test_data_2$time_2, breaks = 5, h = 0.1) #no break points 
+bkpts_2$breakpoints #list of x-values (time) for breakpoints
+
+#-----------------------------------------------------------------
+
+#complete BAR - Variation 0 (Random/Random/Random)
 
 
-#--------------------------------------------------------------------------
 #-------Key:
 # k           = breakpoint's x-axis values 
 # time        = integer x-values of the entire data set 
@@ -24,7 +41,10 @@ points(98,neuron[98,2], col="purple", pch= 18)
 # make        = the proportion (decimal) of the make step to occuring
 # murder      = the proportion (decimal) of the murder step to occuring
 #note: the make and murder need to add to less then one 
-bar0 = function(k, time, data, iterations, make, murder){ 
+#graph        = yes or no to graphing the function 
+bar0 = function(k, time, data, iterations, make, murder, graph){
+  
+  library(MASS)
   
   prob_mmm = c(make, murder) #combining the two probabilties of make and murder that the user specifies 
   
@@ -37,45 +57,49 @@ bar0 = function(k, time, data, iterations, make, murder){
   fitMetrics<-function(k_ends, test_data){
     
     #create sum objects
-    sum_sd = 0
-    sum_SSE = 0
+    sum_loglik = 0
     
-    #get and sum standard deviation and SSE for regressions of all intervals
+    #get and sum log likelihood for regressions of all intervals
     if(length(k_ends) < 3 ){
-      sum_sd = sd(test_data[,2])
       model = lm(test_data[,2]~test_data[,1])
-      SSE = sum(model$residuals^2)
-      sum_SSE = SSE
+      sum_loglik = logLik(model)[1]
     }else{
       for(i in 1:length(k_ends)) {
         if(k_ends[i] != 1){
           min = k_ends[i-1]
-          x_values = test_data[c(min:i),1] #getting the x values in the interval
-          y_values = test_data[c(min:i),2] #getting the y values in the interval
+          x_values = test_data[c(min:k_ends[i]),1] #getting the x values in the interval
+          y_values = test_data[c(min:k_ends[i]),2] #getting the y values in the interval
           data = data.frame(x_values, y_values) #re-making this into a dataframe 
-          sum_sd = sum_sd + sd(y_values) #adding up all the standard deviations
           model = lm(y_values~x_values)
-          SSE = sum(model$residuals^2)
-          sum_SSE = sum_SSE + SSE #adding up all the SSEs 
+          sum_loglik = sum_loglik + logLik(model)[1]
         }
       }
     }
-    c(sum_sd,sum_SSE)
+    return(sum_loglik)
+    
   }
   
+  
   #random make function, this makes a random point 
+  count = 0 
   barMake0<-function(k_ends){
     
-    rand_spot = sample(k_ends[1]:k_ends[length(k_ends)], 1)
-    k_ends_final = sort(c(k_ends, rand_spot))
-    d = diff(k_ends_final)
-    if(min(d) < 3) {
-      barMake0(k_ends)
-    } else {
-      return(k_ends_final)
+    count = count + 1 #this check to make sure we do not get stuck in an infinite loop 
+    if(count < 10 ) {
+      rand_spot = sample(k_ends[1]:k_ends[length(k_ends)], 1) #selects a random spot
+      k_ends_final = sort(c(k_ends, rand_spot)) #adds the random spot and sorts it 
+      d = diff(k_ends_final) #finds the difference between all the spots 
+      if(min(d) < 3) { #this make sure an additional point is not to close to a point already in existance 
+        barMake0(k_ends)
+      } else {
+        return(k_ends_final) #the old breakpoints + the new breakpoints 
+      }
+    }else {
+      return(k_ends)
     }
     
   }
+  
   
   #this function kills one breakpoint randomly 
   barMurder0<-function(k_ends){
@@ -83,7 +107,7 @@ bar0 = function(k, time, data, iterations, make, murder){
     k = k_ends[c(-1,-length(k_ends))]
     random_num = sample(1:length(k), 1)
     k_ends_final = k_ends[-(random_num+1)]
-    k_ends_final
+    return(k_ends_final)
     
   }
   
@@ -92,21 +116,19 @@ bar0 = function(k, time, data, iterations, make, murder){
     
     k_ends_less = barMurder0(k_ends)
     k_ends_final = barMake0(k_ends_less)
-    k_ends_final
+    return(k_ends_final)
     
   }
   
   #initializing matrixes 
-  ratio_data = matrix(NA, nrow=1, ncol=6)
+  ratio_data = matrix(NA, nrow=1, ncol=4)
   all_k_new = matrix(NA, nrow=1, ncol=(n/3))
   all_k_best = matrix(NA, nrow=1, ncol=(n/3))
   
   #Metroplis Hastings 
   for(i in 1:iterations){
     
-    old_metrics = fitMetrics(k_ends, full_data)
-    sigma_old = old_metrics[1]
-    SSE_old = old_metrics[2]
+    old_loglik = fitMetrics(k_ends, full_data)
     
     u_step = runif(1) #random number from 0 to 1 taken from a uniform distribution for selecting step
     
@@ -126,11 +148,9 @@ bar0 = function(k, time, data, iterations, make, murder){
       k_ends_new = barMove0(k_ends)
     }
     
-    new_metrics = fitMetrics(k_ends_new, full_data)
-    sigma_new = new_metrics[1]
-    SSE_new = new_metrics[2]
+    new_loglik = fitMetrics(k_ends_new, full_data)
     
-    ratio = exp((-1*n*log((sqrt(2*pi)*sigma_new)+0.00001)-(1/(2*sigma_new^2+0.00001))*SSE_new)+((n*log(sqrt(2*pi)*sigma_old)+0.00001)-(1/(2*sigma_old^2+0.00001))*SSE_old))
+    ratio = new_loglik - old_loglik
     u_ratio = runif(1) #random number from 0 to 1 taken from a uniform distribution 
     
     if(ratio > u_ratio) {
@@ -145,7 +165,7 @@ bar0 = function(k, time, data, iterations, make, murder){
       k_ends = k_ends
     }
     
-    ratio_data_print = c(ratio, u_ratio, sigma_new, SSE_new, sigma_old, SSE_old)
+    ratio_data_print = c(ratio, u_ratio, old_loglik, new_loglik)
     k_ends_new_print = c(k_ends_new, rep(NA, (n/3)-length(k_ends_new)))
     k_ends_best_print = c(k_ends, rep(NA, (n/3)-length(k_ends)))
     
@@ -157,29 +177,40 @@ bar0 = function(k, time, data, iterations, make, murder){
   
   #cleaning up the matrixs 
   ratio_data = ratio_data[-1,]
+  all_k_new = all_k_new[,-1]
+  all_k_best = all_k_best[,-1]
   all_k_new = all_k_new[-1,colSums(is.na(all_k_new))<nrow(all_k_new)]
   all_k_best = all_k_best[-1,colSums(is.na(all_k_best))<nrow(all_k_best)]
+  clean_max = max(all_k_new[1,], na.rm=TRUE)
+  all_k_new = ifelse(all_k_new == clean_max,NA,all_k_new)
+  all_k_best = ifelse(all_k_best == clean_max,NA,all_k_best)
   
-  #plotting 
-  plot(neuron_time, main = 1, xlab = "Time (ms)", ylab = "Number of Neurons Firing")
-  points(all_k_new[1,],neuron_time[all_k_new[1,],2], col="blue", pch= 16, cex = 2)
-  points(all_k_best[1,],neuron_time[all_k_best[1,],2], col="red", pch= 16, cex=2)
-  for(i in 1:20) {
-    n = i * 5
-    plot(neuron_time, main = n, xlab = "Time (ms)", ylab = "Number of Neurons Firing")
-    points(all_k_new[n,],neuron_time[all_k_new[n,],2], col="blue", pch= 16, cex = 2)
-    points(all_k_best[n,],neuron_time[all_k_best[n,],2], col="red", pch= 16, cex = 2)
+  if(graph == "yes") {
+    #plotting 
+    plot(full_data, main = 1, xlab = "Time")
+    points(all_k_new[1,],full_data[all_k_new[1,],2], col="blue", pch= 16, cex = 2)
+    points(all_k_best[1,],full_data[all_k_best[1,],2], col="red", pch= 16, cex=2)
+    
+    num = iterations / 5
+    for(i in 1:n) {
+      n = i * 5
+      if(is.na(all_k_new[n,]) ) {
+        plot(full_data, main = n, xlab = "Time")
+        points(all_k_best[n,],full_data[all_k_best[n,],2], col="red", pch= 16, cex = 2)
+      } else {
+        plot(full_data, main = n, xlab = "Time")
+        points(all_k_new[n,],full_data[all_k_new[n,],2], col="blue", pch= 16, cex = 2)
+        points(all_k_best[n,],full_data[all_k_best[n,],2], col="red", pch= 16, cex = 2)
+      }
+    }
   }
   
   #prints the results
-  print(ratio_data)
-  print(all_k_new)
-  print(all_k_best)
-  
-  
+  return(list(ratio_data, all_k_new, all_k_best))
   
 }
 
 #calling the function
-bar0(bkpts_neuron$breakpoints, rownames(neuron), neuron$V2, 100, 0.6, 0.2)
+bar_result = bar0(bkpts_2$breakpoints, test_data_2[,1], test_data_2[,2], 20, 0.4, 0.4, "no")
 
+bar0(bkpts_2$breakpoints, test_data_2[,1], test_data_2[,2], 100, 0.4, 0.4, "yes")
