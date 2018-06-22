2e9187e8a0726c0eeeee73353e9add9bee091245
sneitzel <sarahbneitzel.wa@gmail.com>
2018-06-22 16:12:07 -0400

    Interval murder works now.

--- Subtraction/Interval_Subtration.R
+++ Subtraction/Interval_Subtration.R
@@ -1,33 +1,34 @@
-k_ends_test = c(1, 4, 23, 60)
+#subtraction attempt one (deletes break that creates smallest interval)
 
+k_ends_test = c(1, 20, 40, 60, 80)
 
-intervalMurder(k_ends){
+barMurder1 <- function(k_ends){
   
-  all_intv = diff(k_ends) #finds all of the intervals
-  sum_intv = cumsum(all_intv) #finds the sums of the adjacent intervals 
-  small_intv_loc = which(sum_intv == min(sum_intv)) #stores indexes of smallest sum_intv
+	all_intv = diff(k_ends) #finds all of the intervals
+	sum_intv = diff(cumsum(all_intv)) #finds the sums of the adjacent intervals
+	small_intv_loc = which(sum_intv == min(sum_intv)) #stores indexes of smallest sum_intv
+
+	min_intv_loc = 0 #sets the minimum sum_intv index to 0 
   
-  min_intv_loc = 0 #sets the minimum sum_intv index to 0 
+	#if there's only one small sum_intv
+	#{the location in sum_intv is equal to the single value in small_intv_loc}
+	if(length(small_intv_loc) == 1){
+		min_intv_loc = small_intv_loc[1]
+	}
+
+	#if there are multiple small sum_intv
+	#{pick a random number from 1-length of small_intv_loc
+	#location od the smallest interval in sum_intv is equal to the value in small_intv_loc[random#]}
+	else{
+		num = sample(1:length(small_intv_loc), 1)
+		min_intv_loc = small_intv_loc[num]
+	}
   
-  #if there's only one small sum_intv
-  #{the location in sum_intv is equal to the single value in small_intv_loc}
-  if(length(small_intv_loc) == 1){
-    min_intv_loc = small_intv_loc[1]
-  }
-  #if there are multiple small sum_intv
-  #{pick a random number from 1-length of small_intv_loc
-  #location od the smallest interval in sum_intv is equal to the value in small_intv_loc[random#]}
-  else{
-    num = sampple(1:length(small_intv_loc))
-    min_intv_loc = small_intv_loc{num}
-  }
-  
-  #found point we want to delete - might need to check this though 
-  dead_point = k_ends[min_intv_loc+1]
-  
-  #now delete the point 
-  new_set = k_ends[-(dead_point)]
+	#find and delete correct break 
+	new_set = k_ends[-(min_intv_loc+1)]
+
+	return(new_set)
   
 }
 
-intervalMurder(k_ends_test)
+barMurder1(k_ends_test)
