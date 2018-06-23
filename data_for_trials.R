1fbb6e490a1cf91ed50ea242f68878bd9d14da62
Amy Pitts <amy.pitts1@marist.edu>
2018-06-23 00:01:13 -0400

    changing the graphs

--- data_for_trials.R
+++ data_for_trials.R
@@ -1,10 +1,10 @@
 #creating data 
 library("strucchange")
+time = 1:90
 
 
 #----------------ZERO---------------- (no breaks)
 data_0 = rnorm(90, mean = 10, sd = 1)
-time = 1:90
 test_data_0 = data.frame(time, data_0)
 bkpts_0 = breakpoints(test_data_0$data_0 ~ test_data_0$time, breaks = 5, h = 0.1) 
 
@@ -12,8 +12,7 @@ bkpts_0 = breakpoints(test_data_0$data_0 ~ test_data_0$time, breaks = 5, h = 0.1
 first = rnorm(45, mean = 10, sd = 1) 
 second = rnorm(45, mean = 20, sd = 1) 
 data_1 = c(first, second) 
-time = 1:90
-test_data_1 = data.frame(time, dif_means_1) 
+test_data_1 = data.frame(time, data_1) 
 bkpts_1 = breakpoints(test_data_1$data_1 ~ test_data_1$time, breaks = 5, h = 0.1) 
 
 #----------------TWO---------------- (clean sd 2 breaks)
@@ -21,7 +20,6 @@ first = rnorm(30, mean = 5, sd = 1)
 second = rnorm(30, mean = 15, sd = 1) 
 third = rnorm(30, mean = 30, sd = 1) 
 data_2 = c(first, second, third) 
-time = 1:90
 test_data_2 = data.frame(time, data_2) 
 bkpts_2 = breakpoints(test_data_0$data_2 ~ test_data_2$time, breaks = 5, h = 0.1) 
 
@@ -29,7 +27,6 @@ bkpts_2 = breakpoints(test_data_0$data_2 ~ test_data_2$time, breaks = 5, h = 0.1
 first = rnorm(45, mean = 10, sd = 5) 
 second = rnorm(45, mean = 20, sd = 5) 
 data_3 = c(first, second) 
-time = 1:90
 test_data_3 = data.frame(time, data_3) 
 bkpts_3 = breakpoints(test_data_0$data_3 ~ test_data_3$time, breaks = 5, h = 0.1) 
 
@@ -38,7 +35,6 @@ first = rnorm(30, mean = 5, sd = 6)
 second = rnorm(30, mean = 20, sd = 6) 
 third = rnorm(30, mean = 35, sd = 6) 
 data_4 = c(first, second, third) 
-time = 1:90
 test_data_4 = data.frame(time, data_4) 
 bkpts_4 = breakpoints(test_data_4$data_4 ~ test_data_4$time, breaks = 5, h = 0.1)
 
@@ -56,6 +52,7 @@ third = c(seq(149, 382, by = 8))
 data_6 = c(first, second, third)
 test_data_6 = data.frame(time, data_6)
 bkpts_6 = breakpoints(test_data_6$data_6 ~ test_data_6$time, breaks = 5, h = 0.1)
+
 #----------------SEVEN----------------
 first = c(seq(1,23, by = .5))
 second = c(24:68)
@@ -70,4 +67,18 @@ third = c(seq(24, 46, by = .75))
 data_8 = c(first, second, third)
 test_data_8 = data.frame(time, data_8)
 bkpts_8 = breakpoints(test_data_8$data_8 ~ test_data_8$time, breaks = 5, h = 0.1)
+
+
+#plots 
+par(mfrow=c(3,3))
+plot(test_data_0, main = "No Breaks")
+plot(test_data_1, main = "One Break, Small Variance")
+plot(test_data_2, main = "Two Breaks, Small Variance")
+plot(test_data_3, main = "One Break, Big Variance")
+plot(test_data_4, main = "Two Breaks, Big Variance")
+plot(test_data_5, main = "One Break and Big Slopes")
+plot(test_data_6, main = "Two Breaks and Big Slopes ")
+plot(test_data_7, main = "One Break and Small Slopes")
+plot(test_data_8, main = "Two Breaks and Small Slopes")
+par(mfrow=c(1,1))
 
