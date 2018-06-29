91aa24088adffbc133a7e2f52aa55cc9ab6f5ec6
sneitzel <sarahbneitzel.wa@gmail.com>
2018-06-29 11:54:51 -0400

    Added simulations with percents.

--- simulations_with_percents.R
+++ simulations_with_percents.R
@@ -9,10 +9,11 @@ iterations = 2000
 current_bar = bar0 #edit here !!! (which version of bars is being run)
 save_name = "TestList.RData" #edit here !!! (format: "bar#_data#.RData")
 make = 0.3
+percent = 0.03
 
 #Step 4 - run simulation
 
-simulation = function(data_fun, runs, iterations, current_bar, make){
+simulation = function(data_fun, runs, iterations, current_bar, make, percent){
 
 	#initializing storage for returns from all BAR runs
 	current_list = list()
@@ -32,7 +33,7 @@ simulation = function(data_fun, runs, iterations, current_bar, make){
 		current_data = data_fun() 
 		break_p = breakpoints(current_data[,2] ~ current_data[,1], breaks = 5, h = 0.1) 
 		starting_breakpoints = break_p$breakpoints
-		current_result = current_bar(starting_breakpoints, current_data[,1], current_data[,2], iterations, make)
+		current_result = current_bar(starting_breakpoints, current_data[,1], current_data[,2], iterations, make, percent)
 		current_list[[1]] = c(current_list[[1]], current_result$AcceptRate[[1]], recursive = TRUE)
 		current_list[[2]] = cbind(current_list[[2]], current_result$MSE[,1])
 		current_list[[3]] = cbind(current_list[[3]], current_result$Breakpoints)
@@ -45,7 +46,7 @@ simulation = function(data_fun, runs, iterations, current_bar, make){
 	return(current_list)
 }
 
-sim_list = simulation(data_fun, runs, iterations, current_bar, make)
+sim_list = simulation(data_fun, runs, iterations, current_bar, make, percent)
 
 #Step 5 - clean up and define final version of $Breakpoints from simulation results
 
