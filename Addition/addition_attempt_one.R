aa911b1d0b7250923c11a39e6b3be3c783f9926c
Amy Pitts <amy.pitts1@marist.edu>
2018-06-29 15:35:37 -0400

    addition to ^4

--- Addition/addition_attempt_one.R
+++ Addition/addition_attempt_one.R
@@ -6,7 +6,7 @@ k_ends = c(1,30,50,60)
 barMake1<-function(k_ends){
 
   d = diff(k_ends) #finding the distance between all those breakpoints
-  location = rmultinom(1, size = 1, prob = (d^3)/sum(d^3))
+  location = rmultinom(1, size = 1, prob = (d^4)/sum(d^4))
   #locations = which(d == max(d) ) #finding the location(s) of the farthest distance 
   #locations = sample(d[location], 1) #randomly select location in case of equal max distances
   min = k_ends[which.max(location)] #lower bound 
