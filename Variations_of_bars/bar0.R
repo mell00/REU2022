0e498b219b724acf4ff66b4ddc89f2f8a25f6f62
sneitzel <sarahbneitzel.wa@gmail.com>
2018-07-02 14:23:39 -0400

    Fixed q in bar0.

--- Variations_of_bars/bar0.R
+++ Variations_of_bars/bar0.R
@@ -142,26 +142,24 @@ bar0 = function(k, time, data, iterations, make){
     
     old_loglik = fitMetrics(k_ends, full_data) #calls fit matrix to have a function to start with
 
-    make_k = make * min(c(1,dpois(length(k_ends)-1,0.1)/dpois(length(k_ends)-2,0.1)))
-    murder_k =  make * min(c(1,dpois(length(k_ends)-2,0.1)/dpois(length(k_ends)-1,0.1)))
-	print(make_k)
-	print(murder_k)
+    make_k = make * min(1,dpois(length(k_ends)-1,0.1)/dpois(length(k_ends)-2,0.1))
+    murder_k =  make * min(1,dpois(length(k_ends)-2,0.1)/dpois(length(k_ends)-1,0.1))
     
     u_step = runif(1) #random number from 0 to 1 taken from a uniform distribution for selecting step
     
-    if(length(k_ends) < 3 | u_step < make_k){
+    if(length(k_ends) < 3 | u_step <= make_k){
       type = "add"
       a.count = a.count + 1
       k_ends_new = barMake0(k_ends) #make
 
 	    #setting up qs for ratio
-          q1 = murder_k/(length(k_ends_new)-2)
+          q1 = murder_k
 	    full_set = c(k_ends, k_ends[1:length(k_ends)-1]+1, k_ends[1:length(k_ends)-1]+2, k_ends[2:length(k_ends)]-1, k_ends[2:length(k_ends)]-2) #all precluded observations
 	    overlap = sum(table(full_set))-length(table(full_set)) #repeated preclusions
 	    n_free = n - 5*(length(k_ends)-2) - 6 + overlap
-	    q2 = make_k/n_free
+	    q2 = make_k
 
-    } else if(u_step > make_k & u_step < (make_k + murder_k)){
+    } else if(u_step > make_k & u_step <= (make_k + murder_k)){
       type = "sub"
       s.count = s.count + 1
       k_ends_new = barMurder0(k_ends) #murder
@@ -170,8 +168,8 @@ bar0 = function(k, time, data, iterations, make){
 	   full_set = c(k_ends_new, k_ends_new[1:length(k_ends_new)-1]+1, k_ends_new[1:length(k_ends_new)-1]+2, k_ends_new[2:length(k_ends_new)]-1, k_ends_new[2:length(k_ends_new)]-2) #all precluded observations
 	   overlap = sum(table(full_set))-length(table(full_set)) #repeated preclusions
 	   n_free = n - 5*(length(k_ends_new)-2) - 6 + overlap
-	   q1 = make_k/n_free
-	   q2 = murder_k/(length(k_ends)-2)
+	   q1 = make_k
+	   q2 = murder_k
 
     } else{
       type = "move"
@@ -276,6 +274,6 @@ bar0 = function(k, time, data, iterations, make){
 }
 
 #calling the function
-current_result = bar0(bkpts_2$breakpoints, test_data_2[,1], test_data_2[,2], 2000, 0.5)
+current_result = bar0(bkpts_2$breakpoints, test_data_2[,1], test_data_2[,2], 1000, 0.5)
 current_dist = k_dist(current_result$Breakpoints)
 hist(current_dist)
