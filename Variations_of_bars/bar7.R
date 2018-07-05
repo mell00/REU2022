322730526ad78ff41a72a004529d1df2845b17db
Amy Pitts <amy.pitts1@marist.edu>
2018-07-05 14:41:24 -0400

    bar 7

--- Variations_of_bars/bar7.R
+++ Variations_of_bars/bar7.R
@@ -587,6 +587,225 @@ bar7 = function(k, time, data, iterations, make_murder_p, percent){
     
   }
   
+  #additon for the subtraction step
+  #creates list of all of the n_frees woohoo
+  create_n_free_list_sub <- function (data_set, n_not_free_old){
+    n_free_list = setdiff(data_set, n_not_free_old)
+    sort(n_free_list)
+    return (n_free_list)
+  }
+  
+  #creates a list of the scores for the n_frees 
+  n_free_scores_sub <- function(n_free_list, k_ends_new){
+    
+    #sets up global variables
+    k_ends_ix = 2 #index of the closest breakpoint/endpoint in k_ends_old
+    scores_list = c(0)  #establishes the list of scores, will remove 0 later 
+    
+    for(i in 1:length(n_free_list)){#goes through each n_free
+      l = 0 
+      r= 0 
+      min = 0 
+      max = 0 
+      if(n_free_list[i]< k_ends_new[k_ends_ix]){#makes sure that the current element of the k_ends is still the upper bound 
+        l = n_free_list[i]-k_ends_new[k_ends_ix-1] #distance between breakpoint and left neighbor
+        r = k_ends_new[k_ends_ix]-n_free_list[i] #distance between breakpoint and right neighbor
+      }
+      else{#if the current element of the k_ends is not the upper bound, move the upper bound up 
+        k_ends_ix = k_ends_ix+1 
+        l = n_free_list[i]-k_ends_new[k_ends_ix-1] #distance between breakpoint and left neighbor
+        r = k_ends_new[k_ends_ix]-n_free_list[i] #distance between breakpoint and right neighbor
+      }
+      #gives the min and maxes the correct values
+      if(r>l){
+        max = r
+        min = l
+      }else{
+        max = l
+        min = r
+      }
+      scores_list = c(scores_list, min, max) #adds the mins and the maxes to the score_list 
+    }
+    scores_list = scores_list[-1] #takes out that first zero 
+    return(scores_list) 
+    
+  }
+  
+  #calculates the numbers of wins and ties for the added point
+  n_free_win_count_sub <- function(score_list, k_ends_old, k_ends_new){
+    win_counter = 0
+    tie_counter = 0
+    losses_counter = 0 #just to check that I can do addition
+    min_bkpt = 0
+    max_bkpt = 0
+    
+    added_bkpt = setdiff( k_ends_old, k_ends_new) #finds what exactly what the added breakpoint is 
+    add_bkpt_birthplace = 0 #finds location of added break point in the original k_ends
+    for(i in 1:length(k_ends_old)){
+      if (k_ends_old[i] == added_bkpt){
+        add_bkpt_birthplace = i
+      }
+    }
+    
+    #sets the min and max scores for the added breakpoint 
+    left = k_ends_old[add_bkpt_birthplace]-k_ends_old[add_bkpt_birthplace-1]
+    right = k_ends_old[add_bkpt_birthplace+1]-k_ends_old[add_bkpt_birthplace]
+    
+    if(right > left){
+      max_bkpt = right
+      min_bkpt = left
+    }else{
+      max_bkpt = left
+      min_bkpt = right
+    }
+    hasWon = "no" #boolean will check to see if the point has already won match 
+    #goes through the list and counts wins, ties, and losses
+    for(i in 1:length(score_list)){
+      
+      if(i %% 2 != 0){#if i is odd, we're looking at minimums for the first time 
+        hasWon = "no" #just makes sure that hasWon is set to no 
+        if(min_bkpt > score_list[i]){ #if the breakpoint's minimum is less than the scorelist it won minimum 
+          win_counter = win_counter+1 #increase the win counter by 1 
+          hasWon = "yes" #the break point won so nothing else will need to happen 
+        }#if(min_bkpt >= scoring_list[i]) don't do anything 
+      }else if (i %% 2 == 0 && hasWon == "no"){#if i is even and it hasnt won, look at the maximum 
+        if(max_bkpt > score_list[i]){#if the breakpoint's maximum is less than the scorelit it won maximum
+          win_counter = win_counter+1 #increase the win counter by 1 
+        } 
+        else if(max_bkpt == score_list[i]){#if they're equal, then it's a tie
+          tie_counter = tie_counter+0.5 #increase the tie counter by .5 
+        }
+        else{#the breakpoint loss =( so increaste the loss counter 
+          losses_counter = losses_counter +1 #im so sorry for your loss 
+        }
+      }
+    }
+    tie_counter = tie_counter-0.5#with current set up there is an occassion where the point is compared to itself; such a case will result in a tie ; therefore, we need to remove one tie case 
+    total_count = win_counter+tie_counter #now we need to add the total number of wins and ties 
+    return(total_count)
+    
+  }
+  
+  #calculates the second part of the q algorithm for sub
+  part_two_q_add_score_sub <- function(data, n_unfree, k_ends_old, k_ends_new, rhow){
+    
+    list_of_n_frees = create_n_free_list_sub(data, n_unfree) #creates the list of n_free points 
+    n_free_score_list = n_free_scores_sub(list_of_n_frees, k_ends_new) #calculates and creates the scoring list for the n_free points 
+    numerator = n_free_win_count_sub(n_free_score_list, k_ends_old, k_ends_new)
+    denomenator = choose(length(list_of_n_frees), 2)
+    part_2 = (numerator/denomenator)*(1-rhow)+(rhow)*(1/(length(list_of_n_frees)))
+    return(part_2)
+    
+  }
+  
+  #q for additon for addition
+  #creates list of all of the n_frees woohoo
+  create_n_free_list_add <- function (data_set, n_not_free_old){
+    n_free_list = setdiff(data_set, n_not_free_old)
+    sort(n_free_list)
+    return (n_free_list)
+  }
+  
+  #creates a list of the scores for the n_frees 
+  n_free_scores_add <- function(n_free_list, k_ends_old){
+    k_ends_ix = 2 #index of the closest breakpoint/endpoint in k_ends_old
+    scores_list = c(0)  #establishes the list of scores, will remove 0 later 
+    
+    for(i in 1:length(n_free_list)){#goes through each n_free
+      l = 0 
+      r= 0 
+      min = 0 
+      max = 0 
+      if(n_free_list[i]< k_ends_old[k_ends_ix]){#makes sure that the current element of the k_ends is still the upper bound 
+        l = n_free_list[i]-k_ends_old[k_ends_ix-1] #distance between breakpoint and left neighbor
+        r = k_ends_old[k_ends_ix]-n_free_list[i] #distance between breakpoint and right neighbor
+      }
+      else{#if the current element of the k_ends is not the upper bound, move the upper bound up 
+        k_ends_ix = k_ends_ix+1 
+        l = n_free_list[i]-k_ends_old[k_ends_ix-1] #distance between breakpoint and left neighbor
+        r = k_ends_old[k_ends_ix]-n_free_list[i] #distance between breakpoint and right neighbor
+      }
+      #gives the min and maxes the correct values
+      if(r>l){
+        max = r
+        min = l
+      }else{
+        max = l
+        min = r
+      }
+      scores_list = c(scores_list, min, max) #adds the mins and the maxes to the score_list 
+    }
+    scores_list = scores_list[-1] #takes out that first zero 
+    
+    return(scores_list) 
+  }
+  
+  #calculates the numbers of wins and ties for the added point
+  n_free_win_count_add <- function(score_list, k_ends_old, k_ends_new){
+    
+    win_counter = 0
+    tie_counter = 0
+    losses_counter = 0 #just to check that I can do addition
+    min_bkpt = 0
+    max_bkpt = 0
+    added_bkpt = setdiff(k_ends_new, k_ends_old) #finds what exactly what the added breakpoint is 
+    add_bkpt_birthplace = 0 #finds location of added break point in the original k_ends
+    for(i in 1:length(k_ends_new)){
+      if (k_ends_new[i] == added_bkpt){
+        add_bkpt_birthplace = i
+      }
+    }
+    
+    #sets the min and max scores for the added breakpoint 
+    left = k_ends_new[add_bkpt_birthplace]-k_ends_new[add_bkpt_birthplace-1]
+    right = k_ends_new[add_bkpt_birthplace+1]-k_ends_new[add_bkpt_birthplace]
+    if(right > left){
+      max_bkpt = right
+      min_bkpt = left
+    }else{
+      max_bkpt = left
+      min_bkpt = right
+    }
+    
+    hasWon = "no" #boolean will check to see if the point has already won match 
+    
+    #goes through the list and counts wins, ties, and losses
+    for(i in 1:length(score_list)){
+      if(i %% 2 != 0){#if i is odd, we're looking at minimums for the first time 
+        hasWon = "no" #just makes sure that hasWon is set to no
+        if(min_bkpt > score_list[i]){ #if the breakpoint's minimum is less than the scorelist it won minimum 
+          win_counter = win_counter+1 #increase the win counter by 1 
+          hasWon = "yes" #the break point won so nothing else will need to happen 
+        }#if(min_bkpt >= scoring_list[i]) don't do anything 
+      }else if (i %% 2 == 0 && hasWon == "no"){#if i is even and it hasnt won, look at the maximum 
+        if(max_bkpt > score_list[i]){#if the breakpoint's maximum is less than the scorelit it won maximum
+          win_counter = win_counter+1 #increase the win counter by 1 
+        } 
+        else if(max_bkpt == score_list[i]){#if they're equal, then it's a tie
+          tie_counter = tie_counter+0.5 #increase the tie counter by .5 
+        }
+        else{#the breakpoint loss =( so increaste the loss counter 
+          losses_counter = losses_counter +1 #im so sorry for your loss 
+        }
+      }
+    }
+    tie_counter = tie_counter-0.5 #tie case ; we need to remove one tie case
+    total_count = win_counter+tie_counter #now we need to add the total number of wins and ties 
+    return(total_count)
+    
+  }
+  
+  #calculates the second part of the q algorithm for sub
+  part_two_q_add_score_add <- function(data, n_unfree, k_ends_old, k_ends_new, rhow){
+    
+    list_of_n_frees = create_n_free_list_add(data, n_unfree) #creates the list of n_free points 
+    n_free_score_list = n_free_scores_add(list_of_n_frees, k_ends_old) #calculates and creates the scoring list for the n_free points 
+    numerator = n_free_win_count_add(n_free_score_list, k_ends_old, k_ends_new)
+    denomenator = choose(length(list_of_n_frees), 2)
+    part_2 = (numerator/denomenator)*(1-rhow)+(rhow)*(1/(length(list_of_n_frees)))
+    return(part_2)
+    
+  }
   
   #Metroplis Hastings 
   for(i in 1:iterations){
@@ -603,11 +822,8 @@ bar7 = function(k, time, data, iterations, make_murder_p, percent){
       
       #setting up qs for ratio
       full_set = c(k_ends, k_ends[1:length(k_ends)-1]+1, k_ends[1:length(k_ends)-1]+2, k_ends[2:length(k_ends)]-1, k_ends[2:length(k_ends)]-2) #all precluded observations
-      overlap = sum(table(full_set))-length(table(full_set)) #repeated preclusions
-      n_free = n - 5*(length(k_ends)-2) - 6 + overlap
-      n_unfree = setdiff(c(1:n), sort(n_free))
       
-      q2 = make_k * part_two_q_add_score_add(c(1:n), n_unfree, k_ends, k_ends_new, make_k)
+      q2 = make_k * part_two_q_add_score_add( c(1:n), unique(sort(full_set)), k_ends, k_ends_new, make_k)
       q1 = murder_k * part_two_q_sub_score_add(k_ends, k_ends_new, make_k)
       
     } else if(u_step > make_k & u_step <= (make_k + murder_k)){
@@ -619,11 +835,8 @@ bar7 = function(k, time, data, iterations, make_murder_p, percent){
       q2 = murder_k * part_two_q_sub_score_sub(k_ends, k_ends_new, make_k) #changed
       
       full_set = c(k_ends_new, k_ends_new[1:length(k_ends_new)-1]+1, k_ends_new[1:length(k_ends_new)-1]+2, k_ends_new[2:length(k_ends_new)]-1, k_ends_new[2:length(k_ends_new)]-2) #all precluded observations
-      overlap = sum(table(full_set))-length(table(full_set)) #repeated preclusions
-      n_free = n - 5*(length(k_ends_new)-2) - 6 + overlap
-      n_unfree = setdiff(c(1:n), sort(n_free))
       
-      q1 = make_k /(length(k_ends)-2)
+      q1 = make_k * part_two_q_add_score_sub( c(1:n), unique(sort(full_set)), k_ends, k_ends_new,  make_k)
       
     } else{
       move_u = runif(1)
--- q calculations/q_calc_for_addition_score.R
+++ q calculations/q_calc_for_addition_score.R
@@ -1,30 +1,21 @@
-#FUNCTION 1
-#creates list of all of the n_frees woohoo
-#inputs: data set, list of data points that are either breakpoints, endpoints, or in that unacceptable window
 
+#creates list of all of the n_frees woohoo
 create_n_free_list_add <- function (data_set, n_not_free_old){
   n_free_list = setdiff(data_set, n_not_free_old)
   sort(n_free_list)
   return (n_free_list)
 }
 
-
-#FUNCTION 2
 #creates a list of the scores for the n_frees 
 n_free_scores_add <- function(n_free_list, k_ends_old){
-  
-  #sets up global variables
   k_ends_ix = 2 #index of the closest breakpoint/endpoint in k_ends_old
   scores_list = c(0)  #establishes the list of scores, will remove 0 later 
   
   for(i in 1:length(n_free_list)){#goes through each n_free
-    
-    #sets up variables for calculating scores 
     l = 0 
     r= 0 
     min = 0 
     max = 0 
-    
     if(n_free_list[i]< k_ends_old[k_ends_ix]){#makes sure that the current element of the k_ends is still the upper bound 
       l = n_free_list[i]-k_ends_old[k_ends_ix-1] #distance between breakpoint and left neighbor
       r = k_ends_old[k_ends_ix]-n_free_list[i] #distance between breakpoint and right neighbor
@@ -42,30 +33,23 @@ n_free_scores_add <- function(n_free_list, k_ends_old){
       max = l
       min = r
     }
-    #adds the mins and the maxes to the score_list 
-    scores_list = c(scores_list, min, max)
+    scores_list = c(scores_list, min, max) #adds the mins and the maxes to the score_list 
   }
-  #takes out that first zero 
-  scores_list = scores_list[-1]
+  scores_list = scores_list[-1] #takes out that first zero 
   
   return(scores_list) 
 }
 
-#FUNCTION 3
 #calculates the numbers of wins and ties for the added point
 n_free_win_count_add <- function(score_list, k_ends_old, k_ends_new){
   
-  #setting up global variables to use later 
   win_counter = 0
   tie_counter = 0
   losses_counter = 0 #just to check that I can do addition
   min_bkpt = 0
   max_bkpt = 0
-  
-  #finds what exactly what the added breakpoint is 
-  added_bkpt = setdiff(k_ends_new, k_ends_old)
-  #finds location of added break point in the original k_ends
-  add_bkpt_birthplace = 0
+  added_bkpt = setdiff(k_ends_new, k_ends_old) #finds what exactly what the added breakpoint is 
+  add_bkpt_birthplace = 0 #finds location of added break point in the original k_ends
   for(i in 1:length(k_ends_new)){
     if (k_ends_new[i] == added_bkpt){
       add_bkpt_birthplace = i
@@ -87,19 +71,13 @@ n_free_win_count_add <- function(score_list, k_ends_old, k_ends_new){
   
   #goes through the list and counts wins, ties, and losses
   for(i in 1:length(score_list)){
-    
     if(i %% 2 != 0){#if i is odd, we're looking at minimums for the first time 
-      
-      hasWon = "no" #just makes sure that hasWon is set to no 
-      
+      hasWon = "no" #just makes sure that hasWon is set to no
       if(min_bkpt > score_list[i]){ #if the breakpoint's minimum is less than the scorelist it won minimum 
         win_counter = win_counter+1 #increase the win counter by 1 
         hasWon = "yes" #the break point won so nothing else will need to happen 
-        
       }#if(min_bkpt >= scoring_list[i]) don't do anything 
-      
     }else if (i %% 2 == 0 && hasWon == "no"){#if i is even and it hasnt won, look at the maximum 
-      
       if(max_bkpt > score_list[i]){#if the breakpoint's maximum is less than the scorelit it won maximum
         win_counter = win_counter+1 #increase the win counter by 1 
       } 
@@ -111,36 +89,22 @@ n_free_win_count_add <- function(score_list, k_ends_old, k_ends_new){
       }
     }
   }
-  
-  #with current set up there is an occassion where the point is compared to itself
-  #such a case will result in a tie 
-  #therefore, we need to remove one tie case 
-  tie_counter = tie_counter-0.5
-  
-  #now we need to add the total number of wins and ties 
-  total_count = win_counter+tie_counter
-  
+  tie_counter = tie_counter-0.5 #tie case ; we need to remove one tie case
+  total_count = win_counter+tie_counter #now we need to add the total number of wins and ties 
   return(total_count)
+  
 }
 
-
-#FUNCTION 4
 #calculates the second part of the q algorithm for sub
-#inputs: n_free_list, k_ends_new, k_ends_old
-#rhow --- probability of choosing a random addition instead of our created addition 
 part_two_q_add_score_add <- function(data, n_unfree, k_ends_old, k_ends_new, rhow){
-  
-  #creates the list of n_free points 
-  list_of_n_frees = create_n_free_list_add(data, n_unfree)
-  
-  #calculates and creates the scoring list for the n_free points 
-  n_free_score_list = n_free_scores_add(list_of_n_frees, k_ends_old)
-  
+
+  list_of_n_frees = create_n_free_list_add(data, n_unfree) #creates the list of n_free points 
+  n_free_score_list = n_free_scores_add(list_of_n_frees, k_ends_old) #calculates and creates the scoring list for the n_free points 
   numerator = n_free_win_count_add(n_free_score_list, k_ends_old, k_ends_new)
   denomenator = choose(length(list_of_n_frees), 2)
   part_2 = (numerator/denomenator)*(1-rhow)+(rhow)*(1/(length(list_of_n_frees)))
-  
   return(part_2)
+  
 }
 
 #let's test it 
