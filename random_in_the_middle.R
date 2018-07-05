442de3d6a4dab750aefb76653a0aa474a0cf081a
haglichk <khaglich@gmail.com>
2018-07-05 15:21:26 -0400

    added a ceiling so rain wouldn't come in

--- random_in_the_middle.R
+++ random_in_the_middle.R
@@ -5,7 +5,7 @@
 
 random_in_the_middle<-function(x_values){
   n = length(x_values) #Number of observations
-  middle = n/2
+  middle = ceiling(n/2)
   x_values = 1:n #Turn x values into observations (is this needed for this?)
   my_first_breakpoint = c(x_values[middle])
   
