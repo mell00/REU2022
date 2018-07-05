a75165a206181adfcafd4e3b9912fcbefb9c97fc
Amy Pitts <amy.pitts1@marist.edu>
2018-07-05 10:17:36 -0400

    q score

--- q_calc__score_sub_addition.R
+++ q_calc__score_sub_addition.R
@@ -1,35 +1,23 @@
 
-#FUNCTION ONE 
+
 #makes a list of mins and maxes for all of the breakpoints
-#input is the list of breakpoints and endpoints 
-list_of_scores <- function(k_ends){  
+list_of_scores_add <- function(k_ends){  
   
-  #takes the ends off of k_ends so just looking at the breakpoints will be easier
-  k_no_ends = k_ends[-c(1,length(k_ends))]
-  
-  #establishes the list of scores globally
-  scores_list = list()
-  
-  #goes through each breakpoint and finds distances between it and neighbors
-  #concatenates mins and maxes, in order, to scores_list
-  for(i in 1:length(k_no_ends)){
-    
-    #sets up the variables 
+  k_no_ends = k_ends[-c(1,length(k_ends))] #takes the ends off of k_ends so just looking at the breakpoints will be easier
+  scores_list = list() #establishes the list of scores globally
+  for(i in 1:length(k_no_ends)){ #goes through each breakpoint and finds distances between it and neighbors; concatenates mins and maxes, in order, to scores_list
     l = 0 
     r = 0 
     min = 0 
     max = 0 
-    
     if(i == 1){ #first breakpoint has an endpoint as a neighbor
       l = k_no_ends[i] - k_ends[i] #distance between breakpoint and left endpoint
       r = k_no_ends[i+1] - k_no_ends[i] #distance between breakpoint and right neighbor
     }
-    
     else if (i == length(k_no_ends)){#last breakpoint has an endpoint as a neighbor
       l = k_no_ends[i] - k_no_ends[i-1]#distance between breakpoint and left neighbor
       r = k_ends[length(k_ends)] - k_no_ends[i]#distance between breakpoint and right endpoint
     }
-    
     else{#general case 
       l = k_no_ends[i]-k_no_ends[i-1] #distance between breakpoint and left neighbor
       r = k_no_ends[i+1]-k_no_ends[i] #distance between breakpoint and right neighbor
@@ -43,40 +31,32 @@ list_of_scores <- function(k_ends){
       max = l
       min = r
     }
-    
-    #adds the mins and the maxes to the score_list 
-    scores_list = c(scores_list, min, max, recursive=T)
+    scores_list = c(scores_list, min, max, recursive=T)  #adds the mins and the maxes to the score_list 
   }
-  
   return(scores_list) 
+  
 }
 
-#FUNCTION 2 
 #calculates the number of wins and ties for a given breakpoint that might be subtracted 
-#inputs: 
-
-breakpoint_win_count <- function(k_ends_old, k_ends_new){
-  #setting up global variables to use later 
+breakpoint_win_count_add <- function(k_ends_old, k_ends_new){
   win_counter = 0
   tie_counter = 0
   losses_counter = 0 #just to check that I can do addition
   min_bkpt = 0
   max_bkpt = 0
-  
-  #finds what exactly what the delted breakpoint is 
-  dead_bkpt = setdiff(k_ends_old,k_ends_new)
+  dead_bkpt = setdiff(k_ends_new, k_ends_old) #finds what exactly what the delted breakpoint is 
   
   #finds location of deleted break point in the original k_ends
   dead_bkpt_grave = 0
-  for(i in 1:length(k_ends_old)){
-    if (k_ends_old[i] == dead_bkpt){
+  for(i in 1:length(k_ends_new)){
+    if (k_ends_new[i] == dead_bkpt){
       dead_bkpt_grave = i
     }
   }
   
   #sets the min and max scores for the deleted breakpoint 
-  right = k_ends_old[dead_bkpt_grave]-k_ends_old[dead_bkpt_grave-1]
-  left = k_ends_old[dead_bkpt_grave+1]-k_ends_old[dead_bkpt_grave]
+  right = k_ends_new[dead_bkpt_grave]-k_ends_new[dead_bkpt_grave-1]
+  left = k_ends_new[dead_bkpt_grave+1]-k_ends_new[dead_bkpt_grave]
   if(right > left){
     max_bkpt = right
     min_bkpt = left
@@ -85,12 +65,10 @@ breakpoint_win_count <- function(k_ends_old, k_ends_new){
     min_bkpt = right
   }
   
-  scoring_list = list_of_scores(k_ends_old)  #calculates a scoring list for k_ends_old
-  
+  scoring_list = list_of_scores_add(k_ends_new)  #calculates a scoring list for k_ends_new
   hasWon = "no" #boolean will check to see if the point has already won match 
   
-  #goes through the list and counts wins, ties, and losses
-  for(i in 1:length(scoring_list)){
+  for(i in 1:length(scoring_list)){ #goes through the list and counts wins, ties, and losses
     
     if(i %% 2 != 0){#if i is odd, we're looking at minimums for the first time 
       
@@ -115,30 +93,23 @@ breakpoint_win_count <- function(k_ends_old, k_ends_new){
       }
     }
   }
-  
-  #with current set up there is an occassion where the point is compared to itself
-  #such a case will result in a tie 
-  #therefore, we need to remove one tie case 
-  tie_counter = tie_counter-0.5
-  
-  #now we need to add the total number of wins and ties 
-  total_count = win_counter+tie_counter
-  
+  tie_counter = tie_counter-0.5 #taking care of situtation where point is compared to itself therefore, we need to remove one tie case 
+  total_count = win_counter+tie_counter #now we need to add the total number of wins and ties 
   return(total_count)
+  
 }
 
-#FUNCTION THREE 
 #calculates the second part of the q algorithm for sub
-#inputs: k_ends_old, k_ends_new, 
 #rhow --- probability of choosing a random subtraction instead of our created subtraction 
-part_two_q_sub_score <- function(k_ends_old, k_ends_new, rhow){
-  numerator = breakpoint_win_count(k_ends_old, k_ends_new) 
-  denomenator = choose(length(k_ends_old)-2, 2)
-  part_2 = (numerator/denomenator)*(1-rhow) + ((rhow)*(1/(length(k_ends_old)-2)))
+part_two_q_sub_score_add <- function(k_ends_old, k_ends_new, rhow){
+  
+  numerator = breakpoint_win_count_add(k_ends_old, k_ends_new) 
+  denomenator = choose(length(k_ends_new)-2, 2)
+  part_2 = (numerator/denomenator)*(1-rhow) + ((rhow)*(1/(length(k_ends_new)-2)))
   return(part_2)
   
 }
 
-part_two_q_sub_score(c(1,30,44,60,90), c(1,30,60,90), .3)
+
 
 part_two_q_sub_score(c(1,30,60,90), c(1,30,44,60,90), .3)
