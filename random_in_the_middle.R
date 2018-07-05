320520063cf132af8c8df6f6db3553aef6e8eff8
haglichk <khaglich@gmail.com>
2018-07-05 15:12:23 -0400

    initial point in the middle function written

---
+++ random_in_the_middle.R
@@ -0,0 +1,18 @@
+#Places initial knot in the middle of the data set 
+
+#Key
+#x_values = x variable for all observations
+
+random_in_the_middle<-function(x_values){
+  n = length(x_values) #Number of observations
+  middle = n/2
+  x_values = 1:n #Turn x values into observations (is this needed for this?)
+  my_first_breakpoint = c(x_values[middle])
+  
+ # print(my_first_breakpoint)
+  
+  return(my_first_breakpoint)
+}
+
+data_set = c(1:100)
+random_in_the_middle(data_set)
