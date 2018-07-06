cc51409fa31ffb4478f9fa398def47ac6fd640a0
Amy Pitts <amy.pitts1@marist.edu>
2018-07-06 11:16:03 -0400

    bar 4 good

--- Variations_of_bars/bar4.R
+++ Variations_of_bars/bar4.R
@@ -398,10 +398,12 @@ bar4 = function(k, time, data, iterations, make_murder_p, percent, lambda){
     
   }
   
+  
   #makes a list of mins and maxes for all of the breakpoints
   list_of_scores_add <- function(k_ends){  
     k_no_ends = k_ends[-c(1,length(k_ends))] #takes the ends off of k_ends so just looking at the breakpoints will be easier
     scores_list = list() #establishes the list of scores globally
+    
     for(i in 1:length(k_no_ends)){ #goes through each breakpoint and finds distances between it and neighbors; concatenates mins and maxes, in order, to scores_list
       l = 0 
       r = 0 
@@ -409,13 +411,11 @@ bar4 = function(k, time, data, iterations, make_murder_p, percent, lambda){
       max = 0 
       if(i == 1){ #first breakpoint has an endpoint as a neighbor
         l = k_no_ends[i] - k_ends[i] #distance between breakpoint and left endpoint
-        r = k_no_ends[i+1] - k_no_ends[i] #distance between breakpoint and right neighbor
-      }
-      else if (i == length(k_no_ends)){#last breakpoint has an endpoint as a neighbor
+        r = k_ends[i+2] - k_no_ends[i] #distance between breakpoint and right neighbor
+      } else if (i == length(k_no_ends)){#last breakpoint has an endpoint as a neighbor
         l = k_no_ends[i] - k_no_ends[i-1]#distance between breakpoint and left neighbor
         r = k_ends[length(k_ends)] - k_no_ends[i]#distance between breakpoint and right endpoint
-      }
-      else{#general case 
+      } else{#general case 
         l = k_no_ends[i]-k_no_ends[i-1] #distance between breakpoint and left neighbor
         r = k_no_ends[i+1]-k_no_ends[i] #distance between breakpoint and right neighbor
       }
@@ -427,11 +427,11 @@ bar4 = function(k, time, data, iterations, make_murder_p, percent, lambda){
         max = l
         min = r
       }
-      scores_list = c(scores_list, min, max, recursive=T)  #adds the mins and the maxes to the score_list 
-    }
-    return(scores_list) 
-    
+      scores_list = c(scores_list, min, max, recursive=T)  #adds the mins and the maxes to the score_list
+      return(scores_list) 
+    } 
   }
+  
   #calculates the number of wins and ties for a given breakpoint that might be subtracted 
   breakpoint_win_count_add <- function(k_ends_old, k_ends_new){
     win_counter = 0
@@ -440,6 +440,7 @@ bar4 = function(k, time, data, iterations, make_murder_p, percent, lambda){
     min_bkpt = 0
     max_bkpt = 0
     dead_bkpt = setdiff(k_ends_new, k_ends_old) #finds what exactly what the delted breakpoint is 
+    
     #finds location of deleted break point in the original k_ends
     dead_bkpt_grave = 0
     for(i in 1:length(k_ends_new)){
@@ -447,6 +448,7 @@ bar4 = function(k, time, data, iterations, make_murder_p, percent, lambda){
         dead_bkpt_grave = i
       }
     }
+    
     #sets the min and max scores for the deleted breakpoint 
     right = k_ends_new[dead_bkpt_grave]-k_ends_new[dead_bkpt_grave-1]
     left = k_ends_new[dead_bkpt_grave+1]-k_ends_new[dead_bkpt_grave]
@@ -457,6 +459,7 @@ bar4 = function(k, time, data, iterations, make_murder_p, percent, lambda){
       max_bkpt = left
       min_bkpt = right
     }
+    
     scoring_list = list_of_scores_add(k_ends_new)  #calculates a scoring list for k_ends_new
     hasWon = "no" #boolean will check to see if the point has already won match 
     
@@ -490,13 +493,18 @@ bar4 = function(k, time, data, iterations, make_murder_p, percent, lambda){
     return(total_count)
     
   }
+  
   #calculates the second part of the q algorithm for sub
   #rhow --- probability of choosing a random subtraction instead of our created subtraction 
   part_two_q_sub_score_add <- function(k_ends_old, k_ends_new, rhow){
     
     numerator = breakpoint_win_count_add(k_ends_old, k_ends_new) 
     denomenator = choose(length(k_ends_new)-2, 2)
-    part_2 = (numerator/denomenator)*(1-rhow) + ((rhow)*(1/(length(k_ends_new)-2)))
+    if(denomenator == 0 ){
+      part_2 = (0)*(1-rhow) + ((rhow)*(1/(length(k_ends_new)-2)))
+    } else {
+      part_2 = (numerator/denomenator)*(1-rhow) + ((rhow)*(1/(length(k_ends_new)-2)))
+    }
     return(part_2)
     
   }
