f3287b7de658a3b92fc7e25be9473b1a146e6760
sneitzel <40214706+sneitzel@users.noreply.github.com>
2018-07-09 11:59:30 -0400

    Update climateData.R

--- Long_runs/climateData.R
+++ Long_runs/climateData.R
@@ -5,17 +5,17 @@ temp_data[,2] = as.numeric(as.character(temp_data[,2]))
 
 temp_bkpts = breakpoints(temp_data[,2]~temp_data[,1], h=0.05, breaks=10)
 
-temp_results = barB(temp_bkpts$breakpoints, temp_data[,1], temp_data[,2], 2500, 0.5, 0.02)
-hist(temp_results$NumBkpts)
+#temp_results = barB(temp_bkpts$breakpoints, temp_data[,1], temp_data[,2], 2500, 0.5, 0.02)
+#hist(temp_results$NumBkpts)
 
-title = "barB"
+#title = "barB"
 
-if(is.atomic(temp_results$Breakpoints) == TRUE) {
-	hist(temp_results$Breakpoints, main=title, col="red", breaks=138) 
-}else if(dim(temp_results$Breakpoints)[2] >= 2) {
-	column_list = NULL
-	for(i in 1:dim(temp_results$Breakpoints)[2]){
-		column_list = c(column_list, temp_results$Breakpoints[,i], recursive=TRUE)
-	}
-	hist(column_list, main=title, col="red", breaks=138, xlim=c(0,138)) 
-}
+#if(is.atomic(temp_results$Breakpoints) == TRUE) {
+#	hist(temp_results$Breakpoints, main=title, col="red", breaks=138) 
+#}else if(dim(temp_results$Breakpoints)[2] >= 2) {
+#	column_list = NULL
+#	for(i in 1:dim(temp_results$Breakpoints)[2]){
+#		column_list = c(column_list, temp_results$Breakpoints[,i], recursive=TRUE)
+#	}
+#	hist(column_list, main=title, col="red", breaks=138, xlim=c(0,138)) 
+#}
