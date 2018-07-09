8e652110d49a1af29e970f48d81d69f376362ce9
haglichk <40214723+haglichk@users.noreply.github.com>
2018-07-09 11:49:51 -0400

    Add files via upload

---
+++ Long_runs/longrun_10no.R
@@ -0,0 +1,12 @@
+data = test_data_10()[,2]
+time = c(1:length(data))
+library("strucchange")
+baiperron = breakpoints(data ~ time, breaks = 10, h = 0.05)
+bkpts = baiperron$breakpoints
+
+start_time <- Sys.time()
+results = bar0(bkpts, time, data, 1000000, 0.5, 0.02, 1)
+end_time <- Sys.time()
+
+end_time - start_time
+saveRDS(results, file="bar0_data10_bpnoconstraints.RData")
