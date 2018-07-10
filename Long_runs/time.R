3e5a103f9146ba59c6be91047540b879e3ff68dd
sneitzel <40214706+sneitzel@users.noreply.github.com>
2018-07-10 14:00:08 -0400

    Update time.R

--- Long_runs/time.R
+++ Long_runs/time.R
@@ -1,20 +1,20 @@
-bar0_data2_bpno | bar1_data2_bpno | bar2_data2_bpno
-2.775513 hours | 2.240894 hours | 2.934325 hours
-bar0_data2_bpwith | bar1_data2_bpwith | bar2_data2_bpwith
-3.464557 hours | 2.915682 hours | 2.305141 hours
-bar0_data2_middle | bar1_data2_middle | bar2_data2_middle
-3.561302 hours | 2.583613 hours | 2.695248 hours
+#bar0_data2_bpno | bar1_data2_bpno | bar2_data2_bpno
+#2.775513 hours | 2.240894 hours | 2.934325 hours
+#bar0_data2_bpwith | bar1_data2_bpwith | bar2_data2_bpwith
+#3.464557 hours | 2.915682 hours | 2.305141 hours
+#bar0_data2_middle | bar1_data2_middle | bar2_data2_middle
+#3.561302 hours | 2.583613 hours | 2.695248 hours
 
-bar0_data10_bpno | bar1_data10_bpno | bar2_data10_bpno
-3.560775 hours | 2.34281 hours | 2.823501 hours
-bar0_data10_bpwith | bar1_data10_bpno | bar2_data10_bpwith
-3.676524 hours | 2.394353 hours | 2.292334 hours
-bar0_data10_middle | bar2_data10_middle
-2.899285 hours | 3.13553 hours
+#bar0_data10_bpno | bar1_data10_bpno | bar2_data10_bpno
+#3.560775 hours | 2.34281 hours | 2.823501 hours
+#bar0_data10_bpwith | bar1_data10_bpno | bar2_data10_bpwith
+#3.676524 hours | 2.394353 hours | 2.292334 hours
+#bar0_data10_middle | bar1_data10_middle | bar2_data10_middle
+#2.899285 hours | 2.82572 hours | 3.13553 hours
 
-bar0_climate_bpno | bar2_climate_bpno
-5.537377 hours | 4.452221 hours
-bar0_climate_bpwith | bar2_climate_bpwith
-5.412965 hours | 4.503763 hours
-bar0_climate_middle | bar2_climate_middle
-5.534174 hours | 4.266711 hours
+#bar0_climate_bpno | bar2_climate_bpno
+#5.537377 hours | 4.452221 hours
+#bar0_climate_bpwith | bar2_climate_bpwith
+#5.412965 hours | 4.503763 hours
+#bar0_climate_middle | bar2_climate_middle
+#5.534174 hours | 4.266711 hours
