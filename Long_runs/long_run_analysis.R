24ddb47e31166ce12743cce5661c3ca82a202c53
sneitzel <sarahbneitzel.wa@gmail.com>
2018-07-10 18:52:38 -0400

    Breakpoint location analysis for long runs.

--- Long_runs/long_run_analysis.R
+++ Long_runs/long_run_analysis.R
@@ -2,27 +2,27 @@
 bar0_data2_bpnocon = readRDS("bar0_data2_bpnoconstraints.RData")
 bar0_data2_bpcon = readRDS("bar0_data2_bpconstraints.RData")
 bar0_data2_middle = readRDS("bar0_data2_middle.RData")
-#bar1_data2_bpnocon = readRDS("bar1_data2_bpnoconstraints.RData")
-#bar1_data2_bpcon = readRDS("bar1_data2_bpconstraints.RData")
-#bar1_data2_middle = readRDS("bar1_data2_middle.RData")
+bar1_data2_bpnocon = readRDS("bar1_data2_bpnoconstraints.RData")
+bar1_data2_bpcon = readRDS("bar1_data2_bpconstraints.RData")
+bar1_data2_middle = readRDS("bar1_data2_middle.RData")
 bar2_data2_bpnocon = readRDS("bar2_data2_bpnoconstraints.RData")
 bar2_data2_bpcon = readRDS("bar2_data2_bpconstraints.RData")
 bar2_data2_middle = readRDS("bar2_data2_middle.RData")
 bar0_data10_bpnocon = readRDS("bar0_data10_bpnoconstraints.RData")
 bar0_data10_bpcon = readRDS("bar0_data10_bpconstraints.RData")
 bar0_data10_middle = readRDS("bar0_data10_middle.RData")
-#bar1_data10_bpnocon = readRDS("bar1_data10_bpnoconstraints.RData")
-#bar1_data10_bpcon = readRDS("bar1_data10_bpconstraints.RData")
-#bar1_data10_middle = readRDS("bar1_data10_middle.RData")
+bar1_data10_bpnocon = readRDS("bar1_data10_bpnoconstraints.RData")
+bar1_data10_bpcon = readRDS("bar1_data10_bpconstraints.RData")
+bar1_data10_middle = readRDS("bar1_data10_middle.RData")
 bar2_data10_bpnocon = readRDS("bar2_data10_bpnoconstraints.RData")
 bar2_data10_bpcon = readRDS("bar2_data10_bpconstraints.RData")
 bar2_data10_middle = readRDS("bar2_data10_middle.RData")
 bar0_climate_bpnocon = readRDS("bar0_climate_bpnoconstraints.RData")
 bar0_climate_bpcon = readRDS("bar0_climate_bpconstraints.RData")
 bar0_climate_middle = readRDS("bar0_climate_middle.RData")
-#bar1_climate_bpnocon = readRDS("bar1_climate_bpnoconstraints.RData")
-#bar1_climate_bpcon = readRDS("bar1_climate_bpconstraints.RData")
-#bar1_climate_middle = readRDS("bar1_climate_middle.RData")
+bar1_climate_bpnocon = readRDS("bar1_climate_bpnoconstraints.RData")
+bar1_climate_bpcon = readRDS("bar1_climate_bpconstraints.RData")
+bar1_climate_middle = readRDS("bar1_climate_middle.RData")
 bar2_climate_bpnocon = readRDS("bar2_climate_bpnoconstraints.RData")
 bar2_climate_bpcon = readRDS("bar2_climate_bpconstraints.RData")
 bar2_climate_middle = readRDS("bar2_climate_middle.RData")
@@ -34,14 +34,18 @@ acceptrate_bar0 = rbind(bar0_data2_bpnocon$AcceptRate, bar0_data2_bpcon$AcceptRa
 bar0_data2_middle$AcceptRate, bar0_data10_bpnocon$AcceptRate, bar0_data10_bpcon$AcceptRate,
 bar0_data10_middle$AcceptRate, bar0_climate_bpnocon$AcceptRate, bar0_climate_bpcon$AcceptRate,
 bar0_climate_middle$AcceptRate)
+acceptrate_bar1 = rbind(bar1_data2_bpnocon$AcceptRate, bar1_data2_bpcon$AcceptRate,
+bar1_data2_middle$AcceptRate, bar1_data10_bpnocon$AcceptRate, bar1_data10_bpcon$AcceptRate,
+bar1_data10_middle$AcceptRate, bar1_climate_bpnocon$AcceptRate, bar1_climate_bpcon$AcceptRate,
+bar1_climate_middle$AcceptRate)
 acceptrate_bar2 = rbind(bar2_data2_bpnocon$AcceptRate, bar2_data2_bpcon$AcceptRate,
 bar2_data2_middle$AcceptRate, bar2_data10_bpnocon$AcceptRate, bar2_data10_bpcon$AcceptRate,
 bar2_data10_middle$AcceptRate, bar2_climate_bpnocon$AcceptRate, bar2_climate_bpcon$AcceptRate,
 bar2_climate_middle$AcceptRate)
-acceptrate = cbind(acceptrate_bar0, acceptrate_bar2)
+acceptrate = cbind(acceptrate_bar0, acceptrate_bar1, acceptrate_bar2)
 rownames(acceptrate) = c("2bpn", "2bpw", "2mid", "10bpn", "10bpw", "10mid",
 "climbpn", "climbpw", "climmid")
-colnames(acceptrate) = c("bar0", "bar2")
+colnames(acceptrate) = c("bar0", "bar1", "bar2")
 round(acceptrate, 5)
 
 #looking at distribution of number of breakpoints (k)
@@ -58,6 +62,15 @@ num_bar0_1mid = c(mean(bar0_data10_middle$NumBkpts), sd(bar0_data10_middle$NumBk
 num_bar0_cbpn = c(mean(bar0_climate_bpnocon$NumBkpts), sd(bar0_climate_bpnocon$NumBkpts))
 num_bar0_cbpw = c(mean(bar0_climate_bpcon$NumBkpts), sd(bar0_climate_bpcon$NumBkpts))
 num_bar0_cmid = c(mean(bar0_climate_middle$NumBkpts), sd(bar0_climate_middle$NumBkpts))
+num_bar1_2bpn = c(mean(bar1_data2_bpnocon$NumBkpts), sd(bar1_data2_bpnocon$NumBkpts))
+num_bar1_2bpw = c(mean(bar1_data2_bpcon$NumBkpts), sd(bar1_data2_bpcon$NumBkpts))
+num_bar1_2mid = c(mean(bar1_data2_middle$NumBkpts), sd(bar1_data2_middle$NumBkpts))
+num_bar1_1bpn = c(mean(bar1_data10_bpnocon$NumBkpts), sd(bar1_data10_bpnocon$NumBkpts))
+num_bar1_1bpw = c(mean(bar1_data10_bpcon$NumBkpts), sd(bar1_data10_bpcon$NumBkpts))
+num_bar1_1mid = c(mean(bar1_data10_middle$NumBkpts), sd(bar1_data10_middle$NumBkpts))
+num_bar1_cbpn = c(mean(bar1_climate_bpnocon$NumBkpts), sd(bar1_climate_bpnocon$NumBkpts))
+num_bar1_cbpw = c(mean(bar1_climate_bpcon$NumBkpts), sd(bar1_climate_bpcon$NumBkpts))
+num_bar1_cmid = c(mean(bar1_climate_middle$NumBkpts), sd(bar1_climate_middle$NumBkpts))
 num_bar2_2bpn = c(mean(bar2_data2_bpnocon$NumBkpts), sd(bar2_data2_bpnocon$NumBkpts))
 num_bar2_2bpw = c(mean(bar2_data2_bpcon$NumBkpts), sd(bar2_data2_bpcon$NumBkpts))
 num_bar2_2mid = c(mean(bar2_data2_middle$NumBkpts), sd(bar2_data2_middle$NumBkpts))
@@ -71,12 +84,14 @@ num_bar2_cmid = c(mean(bar2_climate_middle$NumBkpts), sd(bar2_climate_middle$Num
 ###create single data frame which shows mean and SD for all variations
 num_bar0 = rbind(num_bar0_2bpn, num_bar0_2bpw, num_bar0_2mid, num_bar0_1bpn,
 num_bar0_1bpw, num_bar0_1mid, num_bar0_cbpn, num_bar0_cbpw, num_bar0_cmid)
+num_bar1 = rbind(num_bar1_2bpn, num_bar1_2bpw, num_bar1_2mid, num_bar1_1bpn,
+num_bar1_1bpw, num_bar1_1mid, num_bar1_cbpn, num_bar1_cbpw, num_bar1_cmid)
 num_bar2 = rbind(num_bar2_2bpn, num_bar2_2bpw, num_bar2_2mid, num_bar2_1bpn,
 num_bar2_1bpw, num_bar2_1mid, num_bar2_cbpn, num_bar2_cbpw, num_bar2_cmid)
-num = cbind(num_bar0, num_bar2)
+num = cbind(num_bar0, num_bar1, num_bar2)
 rownames(num) = c("2bpn", "2bpw", "2mid", "10bpn", "10bpw", "10mid",
 "climbpn", "climbpw", "climmid")
-colnames(num) = c("bar0 Mean", "bar0 SD", "bar2 Mean", "bar2 SD")
+colnames(num) = c("bar0 Mean", "bar0 SD", "bar1 Mean", "bar1 SD", "bar2 Mean", "bar2 SD")
 round(num, 5)
 
 ###set graphical parameters
@@ -171,6 +186,17 @@ rownames(bkpts) = c("bar0", "bar1", "bar2")
 colnames(bkpts) = c("Breakpoints")
 bkpts
 
+
+##function to look at modes
+modefunc <- function(x){
+    tabresult <- tabulate(x)
+    themode <- which(tabresult == max(tabresult))
+    if(sum(tabresult == max(tabresult))>1) themode <- NA
+    return(themode)
+}
+
+apply(na.omit(bar2_climate_bpnocon$Breakpoints), 2, modefunc)
+
 ##graphically
 
 ###function to plot frequency of breakpoints
@@ -187,17 +213,17 @@ plot_tau_hist <- function(breakpoints, title, x_axis_lab, y_axis_lab, color, num
 	}
 
 }
-
+par(mfrow=c(3,3))
 ###specify which simulation you want to look at right now
 current_sim = bar2_climate_middle$Breakpoints
-current_title = "bar2_climate_middle"
+current_title = "BAR 2 - Middle Starting Point"
 ###specify correct locations of breakpoints for this training set
 current_true = c()
 
 ###set graphical parameters
 x_label = "Location of Breakpoints"
 y_label = "Number of Iterations"
-n_breaks = c(1:138)
+n_breaks = c(seq(1,138,2))
 x_limits = c(1,138)
 y_lower = 0
 y_upper = 100000
@@ -207,6 +233,7 @@ line_type = 2
 line_width = 1
 
 ###plot distributions of breakpoint locations
-plot_tau_hist(current_sim, current_title, x_label, y_label, bar_color, n_breaks, x_limits, c(y_lower, y_upper))
+plot_tau_hist(current_sim, current_title, x_label, y_label, bar_color,
+n_breaks, x_limits, c(y_lower, y_upper))
 if(length(current_true)>=1){lines(c(current_true[[1]],current_true[[1]]),c(y_lower,y_upper),col=line_color, lty=line_type, lwd=line_width)}
 if(length(current_true)>=2){lines(c(current_true[[2]],current_true[[2]]),c(y_lower,y_upper),col=line_color, lty=line_type, lwd=line_width)}
--- Long_runs/time.R
+++ Long_runs/time.R
@@ -18,3 +18,31 @@
 #5.412965 hours | 3.820345 hours | 4.503763 hours
 #bar0_climate_middle | bar1_climate_middle | bar2_climate_middle
 #5.534174 hours | 3.820418 hours | 4.266711 hours
+
+time_data2_bpno = cbind(2.775513, 2.240894, 2.934325)
+time_data2_bpwith = cbind(3.464557, 2.915682, 2.305141)
+time_data_middle = cbind(3.561302, 2.583613, 2.695248)
+
+time_data10_bpno = cbind(3.560775, 2.34281, 2.823501)
+time_data10_bpwith = cbind(3.676524, 2.394353, 2.292334)
+time_data10_middle = cbind(2.899285, 2.82572, 3.13553)
+
+time_climate_bpno = cbind(5.537377, 3.766292, 4.452221)
+time_climate_bpwith = cbind(5.412965, 3.820345, 4.503763)
+time_climate_middle = cbind(5.534174, 3.820418, 4.266711)
+
+time = rbind(time_data2_bpno, time_data2_bpwith, time_data_middle,
+time_data10_bpno, time_data10_bpwith, time_data10_middle,
+time_climate_bpno, time_climate_bpwith, time_climate_middle)
+
+time_means = colMeans(time)
+time_sd = apply(time, 2, sd)
+time_ci = NULL
+
+for(i in 1:3){
+current = rbind(time_means[i]+2*time_sd[i], time_means[i], time_means[i]-2*time_sd[i])
+time_ci = cbind(time_ci, current)
+}
+rownames(time_ci)=c("Upper", "Mean", "Lower")
+colnames(time_ci)=c("bar0", "bar1", "bar2")
+time_ci
