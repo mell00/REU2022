b493f4c43891c68d614ef1c330ca9313ccbdef3f
Amy Pitts <amy.pitts1@marist.edu>
2018-07-10 17:47:32 -0400

    vis

--- long_runs_anaylsis.R
+++ long_runs_anaylsis.R
@@ -220,75 +220,451 @@ legend(1, .5, legend=c("Bai-Perron Constrains","Bai-Perron No Constrains","Middl
 #bar0
 
 #mCbar0_bp
-plot(bar0_climate_bpconstraints$MSE[,1], ylab = "MSE", xlab="Number of Iterations")
+plot(bar0_climate_bpconstraints$MSE[c(1:1000),1], ylab = "MSE", xlab="Number of Iterations")
+#mCbar0_bpno#mCbar0_bp
+plot(bar0_climate_bpconstraints$MSE[c(1:1000),1], ylab = "MSE", xlab="Number of Iterations")
 #mCbar0_bpno
-plot(bar0_climate_bpnoconstraints$MSE[,1], ylab = "MSE", xlab="Number of Iterations")
+plot(bar0_climate_bpnoconstraints$MSE[c(1:1000),1], ylab = "MSE", xlab="Number of Iterations")
 #mCbar0_mid
-plot(bar0_climate_middle$MSE[,1], ylab = "MSE", xlab="Number of Iterations")
+plot(bar0_climate_middle$MSE[c(1:1000),1], ylab = "MSE", xlab="Number of Iterations")
 
 #m10bar0_bp
-plot(bar0_data10_bpconstraints$MSE[,1], ylab = "MSE", xlab="Number of Iterations")
+plot(bar0_data10_bpconstraints$MSE[c(1:1000),1], ylab = "MSE", xlab="Number of Iterations")
 #m10bar0_bpno 
-plot(bar0_data10_bpnoconstraints$MSE[,1], ylab = "MSE", xlab="Number of Iterations")
+plot(bar0_data10_bpnoconstraints$MSE[c(1:1000),1], ylab = "MSE", xlab="Number of Iterations")
 #m10bar0_mid 
-plot(bar0_data10_middle$MSE[,1], ylab = "MSE", xlab="Number of Iterations")
+plot(bar0_data10_middle$MSE[c(1:1000),1], ylab = "MSE", xlab="Number of Iterations")
 
 #m2bar0_bp 
-plot(bar0_data2_bpconstraints$MSE[,1], ylab = "MSE", xlab="Number of Iterations")
+plot(bar0_data2_bpconstraints$MSE[c(1:1000),1], ylab = "MSE", xlab="Number of Iterations")
 #m2bar0_bpno 
-plot(bar0_data2_bpnoconstraints$MSE[,1], ylab = "MSE", xlab="Number of Iterations")
+plot(bar0_data2_bpnoconstraints$MSE[c(1:1000),1], ylab = "MSE", xlab="Number of Iterations")
 #m2bar0_mid 
-plot(bar0_data2_middle$MSE[,1], ylab = "MSE", xlab="Number of Iterations")
+plot(bar0_data2_middle$MSE[c(1:1000),1], ylab = "MSE", xlab="Number of Iterations")
 
 
 #bar1
 
 #mCbar1_bp 
-plot(bar1_climate_bpconstraints$MSE[,1], ylab = "MSE", xlab="Number of Iterations")
+plot(bar1_climate_bpconstraints$MSE[c(1:1000),1], ylab = "MSE", xlab="Number of Iterations")
 #mCbar1_bpno
-plot(bar1_climate_bpnoconstraints$MSE[,1], ylab = "MSE", xlab="Number of Iterations")
+plot(bar1_climate_bpnoconstraints$MSE[c(1:1000),1], ylab = "MSE", xlab="Number of Iterations")
 #mCbar1_mid 
-plot(bar1_climate_middle$MSE[,1], ylab = "MSE", xlab="Number of Iterations")
+plot(bar1_climate_middle$MSE[c(1:1000),1], ylab = "MSE", xlab="Number of Iterations")
 
 #m10bar1_bp 
-plot(bar1_data10_bpconstraints$MSE[,1], ylab = "MSE", xlab="Number of Iterations")
+plot(bar1_data10_bpconstraints$MSE[c(1:1000),1], ylab = "MSE", xlab="Number of Iterations")
 #m10bar1_bpno 
-plot(bar1_data10_bpnoconstraints$MSE[,1], ylab = "MSE", xlab="Number of Iterations")
+plot(bar1_data10_bpnoconstraints$MSE[c(1:1000),1], ylab = "MSE", xlab="Number of Iterations")
 #m10bar1_mid
-plot(bar1_data10_middle$MSE[,1], ylab = "MSE", xlab="Number of Iterations")
+plot(bar1_data10_middle$MSE[c(1:1000),1], ylab = "MSE", xlab="Number of Iterations")
 
 #m2bar1_bp 
-plot(bar1_data2_bpconstraints$MSE[,1], ylab = "MSE", xlab="Number of Iterations")
+plot(bar1_data2_bpconstraints$MSE[c(1:1000),1], ylab = "MSE", xlab="Number of Iterations")
 #m2bar1_bpno
-plot(bar1_data2_bpnoconstraints$MSE[,1], ylab = "MSE", xlab="Number of Iterations")
+plot(bar1_data2_bpnoconstraints$MSE[c(1:1000),1], ylab = "MSE", xlab="Number of Iterations")
 #m2bar1_mid 
-plot(bar1_data2_middle$MSE[,1], ylab = "MSE", xlab="Number of Iterations")
+plot(bar1_data2_middle$MSE[c(1:1000),1], ylab = "MSE", xlab="Number of Iterations")
 
 #bar2
 
 #mCbar2_bp
-plot(bar2_climate_bpconstraints$MSE[,1], ylab = "MSE", xlab="Number of Iterations")
+plot(bar2_climate_bpconstraints$MSE[c(1:1000),1], ylab = "MSE", xlab="Number of Iterations")
 #mCbar2_bpno
-plot(bar2_climate_bpnoconstraints$MSE[,1], ylab = "MSE", xlab="Number of Iterations")
+plot(bar2_climate_bpnoconstraints$MSE[c(1:1000),1], ylab = "MSE", xlab="Number of Iterations")
 #mCbar2_mid
-plot(bar2_climate_middle$MSE[,1], ylab = "MSE", xlab="Number of Iterations")
+plot(bar2_climate_middle$MSE[c(1:1000),1], ylab = "MSE", xlab="Number of Iterations")
 
 #m10bar2_bp 
-plot(bar2_data10_bpconstraints$MSE[,1], ylab = "MSE", xlab="Number of Iterations")
+plot(bar2_data10_bpconstraints$MSE[c(1:1000),1], ylab = "MSE", xlab="Number of Iterations")
 #m10bar2_bpno 
-plot(bar2_data10_bpnoconstraints$MSE[,1], ylab = "MSE", xlab="Number of Iterations")
+plot(bar2_data10_bpnoconstraints$MSE[c(1:1000),1], ylab = "MSE", xlab="Number of Iterations")
 #m10bar2_mid 
-plot(bar2_data10_middle$MSE[,1], ylab = "MSE", xlab="Number of Iterations")
+plot(bar2_data10_middle$MSE[c(1:1000),1], ylab = "MSE", xlab="Number of Iterations")
 
 #m2bar2_bp
-plot(bar2_data2_bpconstraints$MSE[,1], ylab = "MSE", xlab="Number of Iterations")
+plot(bar2_data2_bpconstraints$MSE[c(1:1000),1], ylab = "MSE", xlab="Number of Iterations")
 #m2bar2_bpno
-plot(bar2_data2_bpnoconstraints$MSE[,1], ylab = "MSE", xlab="Number of Iterations")
+plot(bar2_data2_bpnoconstraints$MSE[c(1:1000),1], ylab = "MSE", xlab="Number of Iterations")
 #m2bar2_mid 
-plot(bar2_data2_middle$MSE[,1], ylab = "MSE", xlab="Number of Iterations")
+plot(bar2_data2_middle$MSE[c(1:1000),1], ylab = "MSE", xlab="Number of Iterations")
 
 
 
 
+
+#Bar 0 
+#mCbar0_bp
+plot(bar0_climate_bpconstraints$MSE[c(1:2000),1], ylab = "MSE", xlab="Number of Iterations")
+    #about 500 
+#mCbar0_bpno
+plot(bar0_climate_bpnoconstraints$MSE[c(1:1000),1], ylab = "MSE", xlab="Number of Iterations")
+    #about 400
+#mCbar0_mid
+plot(bar0_climate_middle$MSE[c(1:10000),1], ylab = "MSE", xlab="Number of Iterations")
+    #ABOut 2000
+
+#m10bar0_bp
+plot(bar0_data10_bpconstraints$MSE[c(1:1000),1], ylab = "MSE", xlab="Number of Iterations")
+    ##about 100
+#m10bar0_bpno 
+plot(bar0_data10_bpnoconstraints$MSE[c(1:1000),1], ylab = "MSE", xlab="Number of Iterations")
+    #ABout 200
+#m10bar0_mid 
+plot(bar0_data10_middle$MSE[c(1:1000),1], ylab = "MSE", xlab="Number of Iterations")
+    ##About 300
+
+#m2bar0_bp 
+plot(bar0_data2_bpconstraints$MSE[c(1:1000),1], ylab = "MSE", xlab="Number of Iterations")
+    #Anout 50
+#m2bar0_bpno 
+plot(bar0_data2_bpnoconstraints$MSE[c(1:1000),1], ylab = "MSE", xlab="Number of Iterations")
+    ##None
+#m2bar0_mid 
+plot(bar0_data2_middle$MSE[c(1:1000),1], ylab = "MSE", xlab="Number of Iterations")
+    ##About 50
+
+
+#bar1
+
+#mCbar1_bp 
+plot(bar1_climate_bpconstraints$MSE[c(1:2000),1], ylab = "MSE", xlab="Number of Iterations")
+    ##about 1000
+#mCbar1_bpno
+plot(bar1_climate_bpnoconstraints$MSE[c(1:1000),1], ylab = "MSE", xlab="Number of Iterations")
+    ##About 1500
+#mCbar1_mid 
+plot(bar1_climate_middle$MSE[c(0:1000),1], ylab = "MSE", xlab="Number of Iterations")
+    ##About 1000
+
+#m10bar1_bp 
+plot(bar1_data10_bpconstraints$MSE[c(1:1000),1], ylab = "MSE", xlab="Number of Iterations")
+    ##About 100
+#m10bar1_bpno 
+plot(bar1_data10_bpnoconstraints$MSE[c(1:1000),1], ylab = "MSE", xlab="Number of Iterations")
+    #Maybe 50
+#m10bar1_mid
+plot(bar1_data10_middle$MSE[c(1:1000),1], ylab = "MSE", xlab="Number of Iterations")
+    ##Maybe 20
+
+#m2bar1_bp 
+plot(bar1_data2_bpconstraints$MSE[c(1:1000),1], ylab = "MSE", xlab="Number of Iterations")
+    #Maybe 50
+#m2bar1_bpno
+plot(bar1_data2_bpnoconstraints$MSE[c(1:1000),1], ylab = "MSE", xlab="Number of Iterations")
+    ##None
+#m2bar1_mid 
+plot(bar1_data2_middle$MSE[c(1:1000),1], ylab = "MSE", xlab="Number of Iterations")
+    ##Around 100
+
+
+#bar2
+
+#mCbar2_bp
+plot(bar2_climate_bpconstraints$MSE[c(1:1000),1], ylab = "MSE", xlab="Number of Iterations")
+    ##Around 500
+#mCbar2_bpno
+plot(bar2_climate_bpnoconstraints$MSE[c(1:2000),1], ylab = "MSE", xlab="Number of Iterations")
+    #All over the place 900
+#mCbar2_mid
+plot(bar2_climate_middle$MSE[c(1:3000),1], ylab = "MSE", xlab="Number of Iterations")
+    #All over the place ##Around 1250
+
+#m10bar2_bp 
+plot(bar2_data10_bpconstraints$MSE[c(1:1000),1], ylab = "MSE", xlab="Number of Iterations")
+    ##Around 700
+#m10bar2_bpno 
+plot(bar2_data10_bpnoconstraints$MSE[c(1:1000),1], ylab = "MSE", xlab="Number of Iterations")
+    ##Around 50 ish 
+#m10bar2_mid 
+plot(bar2_data10_middle$MSE[c(1:4000),1], ylab = "MSE", xlab="Number of Iterations")
+    ##Around 200 
+
+#m2bar2_bp
+plot(bar2_data2_bpconstraints$MSE[c(1:3000),1], ylab = "MSE", xlab="Number of Iterations")
+    #Around 50 or 500
+#m2bar2_bpno
+plot(bar2_data2_bpnoconstraints$MSE[c(1:1000),1], ylab = "MSE", xlab="Number of Iterations")
+    ##Around 500
+#m2bar2_mid 
+plot(bar2_data2_middle$MSE[c(1:3000),1], ylab = "MSE", xlab="Number of Iterations")
+    ##Around 50 or 500
+
+
+
+
+
+
+#-------------------------------------BIC---------------------------------#
+#bar0
+
+
+#mCbar0_bpno
+plot(bar0_climate_bpconstraints$BIC[c(1:25000),1], ylab = "BIC", xlab="Number of Iterations")
+    ##About 6000
+#mCbar0_bpno
+plot(bar0_climate_bpnoconstraints$BIC[c(1:10000),1], ylab = "BIC", xlab="Number of Iterations")
+    ##ABOUT 5000
+#mCbar0_mid
+plot(bar0_climate_middle$BIC[c(1:10000),1], ylab = "BIC", xlab="Number of Iterations")
+    ##About 1000
+
+#m10bar0_bp
+plot(bar0_data10_bpconstraints$BIC[c(1:1000),1], ylab = "BIC", xlab="Number of Iterations")
+    ##about 100
+#m10bar0_bpno 
+plot(bar0_data10_bpnoconstraints$BIC[c(1:2000),1], ylab = "BIC", xlab="Number of Iterations")
+    ##about 400
+#m10bar0_mid 
+plot(bar0_data10_middle$BIC[c(1:1000),1], ylab = "BIC", xlab="Number of Iterations")
+    ##about 200
+
+#m2bar0_bp 
+plot(bar0_data2_bpconstraints$BIC[c(1:1000),1], ylab = "BIC", xlab="Number of Iterations")
+    ##ABout 400
+#m2bar0_bpno 
+plot(bar0_data2_bpnoconstraints$BIC[c(1:10000),1], ylab = "BIC", xlab="Number of Iterations")
+    ##None 
+#m2bar0_mid 
+plot(bar0_data2_middle$BIC[c(1:1000),1], ylab = "BIC", xlab="Number of Iterations")
+    ##About 400
+
+#bar1
+
+#mCbar1_bp 
+plot(bar1_climate_bpconstraints$BIC[c(1:1000),1], ylab = "BIC", xlab="Number of Iterations")
+#mCbar1_bpno
+plot(bar1_climate_bpnoconstraints$BIC[c(1:1000),1], ylab = "BIC", xlab="Number of Iterations")
+#mCbar1_mid 
+plot(bar1_climate_middle$BIC[c(1:1000),1], ylab = "BIC", xlab="Number of Iterations")
+
+#m10bar1_bp 
+plot(bar1_data10_bpconstraints$BIC[c(1:1000),1], ylab = "BIC", xlab="Number of Iterations")
+#m10bar1_bpno 
+plot(bar1_data10_bpnoconstraints$BIC[c(1:1000),1], ylab = "BIC", xlab="Number of Iterations")
+#m10bar1_mid
+plot(bar1_data10_middle$BIC[c(1:1000),1], ylab = "BIC", xlab="Number of Iterations")
+
+#m2bar1_bp 
+plot(bar1_data2_bpconstraints$BIC[c(1:1000),1], ylab = "BIC", xlab="Number of Iterations")
+#m2bar1_bpno
+plot(bar1_data2_bpnoconstraints$BIC[c(1:1000),1], ylab = "BIC", xlab="Number of Iterations")
+#m2bar1_mid 
+plot(bar1_data2_middle$BIC[c(1:1000),1], ylab = "BIC", xlab="Number of Iterations")
+
+#bar2
+
+#mCbar2_bp
+plot(bar2_climate_bpconstraints$BIC[c(1:3000),1], ylab = "BIC", xlab="Number of Iterations")
+    #about 1000
+#mCbar2_bpno
+plot(bar2_climate_bpnoconstraints$BIC[c(1:10000),1], ylab = "BIC", xlab="Number of Iterations")
+    #about 1000
+#mCbar2_mid
+plot(bar2_climate_middle$BIC[c(1:3000),1], ylab = "BIC", xlab="Number of Iterations")
+    #about 1000
+
+#m10bar2_bp 
+plot(bar2_data10_bpconstraints$BIC[c(1:3000),1], ylab = "BIC", xlab="Number of Iterations")
+    #about 1500
+#m10bar2_bpno 
+plot(bar2_data10_bpnoconstraints$BIC[c(1:4000),1], ylab = "BIC", xlab="Number of Iterations")
+    #about 1000
+#m10bar2_mid 
+plot(bar2_data10_middle$BIC[c(1:1000),1], ylab = "BIC", xlab="Number of Iterations")
+    #about 500
+
+#m2bar2_bp
+plot(bar2_data2_bpconstraints$BIC[c(1:1000),1], ylab = "BIC", xlab="Number of Iterations")
+    ##about 500
+#m2bar2_bpno
+plot(bar2_data2_bpnoconstraints$BIC[c(1:1000),1], ylab = "BIC", xlab="Number of Iterations")
+    ##about 500
+#m2bar2_mid 
+plot(bar2_data2_middle$BIC[c(1:2000),1], ylab = "BIC", xlab="Number of Iterations")
+    #about 200
+
+
+
+##----------------------more BIC------------------------------
+#-----------BIC-----
+#bar0
+bCbar0_bp = mean(bar0_climate_bpconstraints$BIC[,1])
+bCbar0_bpno = mean(bar0_climate_bpnoconstraints$BIC[,1])
+bCbar0_mid = mean(bar0_climate_middle$BIC[,1])
+
+b10bar0_bp = mean(bar0_data10_bpconstraints$BIC[,1])
+b10bar0_bpno = mean(bar0_data10_bpnoconstraints$BIC[,1])
+b10bar0_mid = mean(bar0_data10_middle$BIC[,1])
+
+b2bar0_bp = mean(bar0_data2_bpconstraints$BIC[,1])
+b2bar0_bpno = mean(bar0_data2_bpnoconstraints$BIC[,1])
+b2bar0_mid = mean(bar0_data2_middle$BIC[,1])
+
+sdBCbar0_bp = sd(bar0_climate_bpconstraints$BIC[,1])
+sdBCbar0_bpno = sd(bar0_climate_bpnoconstraints$BIC[,1])
+sdBCbar0_mid = sd(bar0_climate_middle$BIC[,1])
+
+sdB10bar0_bp = sd(bar0_data10_bpconstraints$BIC[,1])
+sdB10bar0_bpno = sd(bar0_data10_bpnoconstraints$BIC[,1])
+sdB10bar0_mid = sd(bar0_data10_middle$BIC[,1])
+
+sdB2bar0_bp = sd(bar0_data2_bpconstraints$BIC[,1])
+sdB2bar0_bpno = sd(bar0_data2_bpnoconstraints$BIC[,1])
+sdB2bar0_mid = sd(bar0_data2_middle$BIC[,1])
+
+
+#bar1
+bCbar1_bp = mean(bar1_climate_bpconstraints$BIC[,1])
+bCbar1_bpno = mean(bar1_climate_bpnoconstraints$BIC[,1])
+bCbar1_mid = mean(bar1_climate_middle$BIC[,1])
+
+b10bar1_bp = mean(bar1_data10_bpconstraints$BIC[,1])
+b10bar1_bpno = mean(bar1_data10_bpnoconstraints$BIC[,1])
+b10bar1_mid = mean(bar1_data10_middle$BIC[,1])
+
+b2bar1_bp = mean(bar1_data2_bpconstraints$BIC[,1])
+b2bar1_bpno = mean(bar1_data2_bpnoconstraints$BIC[,1])
+b2bar1_mid = mean(bar1_data2_middle$BIC[,1])
+
+sdBCbar1_bp = sd(bar1_climate_bpconstraints$BIC[,1])
+sdBCbar1_bpno = sd(bar1_climate_bpnoconstraints$BIC[,1])
+sdBCbar1_mid = sd(bar1_climate_middle$BIC[,1])
+
+sdB10bar1_bp = sd(bar1_data10_bpconstraints$BIC[,1])
+sdB10bar1_bpno = sd(bar1_data10_bpnoconstraints$BIC[,1])
+sdB10bar1_mid = sd(bar1_data10_middle$BIC[,1])
+
+sdB2bar1_bp = sd(bar1_data2_bpconstraints$BIC[,1])
+sdB2bar1_bpno = sd(bar1_data2_bpnoconstraints$BIC[,1])
+sdB2bar1_mid = sd(bar1_data2_middle$BIC[,1])
+
+
+#bar2
+bCbar2_bp = mean(bar2_climate_bpconstraints$BIC[,1])
+bCbar2_bpno = mean(bar2_climate_bpnoconstraints$BIC[,1])
+bCbar2_mid = mean(bar2_climate_middle$BIC[,1])
+
+b10bar2_bp = mean(bar2_data10_bpconstraints$BIC[,1])
+b10bar2_bpno = mean(bar2_data10_bpnoconstraints$BIC[,1])
+b10bar2_mid = mean(bar2_data10_middle$BIC[,1])
+
+b2bar2_bp = mean(bar2_data2_bpconstraints$BIC[,1])
+b2bar2_bpno = mean(bar2_data2_bpnoconstraints$BIC[,1])
+b2bar2_mid = mean(bar2_data2_middle$BIC[,1])
+
+sdBCbar2_bp = sd(bar2_climate_bpconstraints$BIC[,1])
+sdBCbar2_bpno = sd(bar2_climate_bpnoconstraints$BIC[,1])
+sdBCbar2_mid = sd(bar2_climate_middle$BIC[,1])
+
+sdB10bar2_bp = sd(bar2_data10_bpconstraints$BIC[,1])
+sdB10bar2_bpno = sd(bar2_data10_bpnoconstraints$BIC[,1])
+sdB10bar2_mid = sd(bar2_data10_middle$BIC[,1])
+
+sdB2bar2_bp = sd(bar2_data2_bpconstraints$BIC[,1])
+sdB2bar2_bpno = sd(bar2_data2_bpnoconstraints$BIC[,1])
+sdB2bar2_mid = sd(bar2_data2_middle$BIC[,1])
+
+
+b_C_bp = c(bCbar0_bp, bCbar1_bp, bCbar2_bp)
+b_C_bpno = c(bCbar0_bpno, bCbar1_bpno, bCbar2_bpno)
+b_C_mid = c(bCbar0_mid, bCbar1_mid, bCbar2_mid)
+
+sdB_C_bp = c(sdBCbar0_bp, sdBCbar1_bp, sdBCbar2_bp)
+sdB_C_bpno = c(sdBCbar0_bpno, sdBCbar1_bpno, sdBCbar2_bpno)
+sdB_C_mid = c(sdBCbar0_mid, sdBCbar1_mid, sdBCbar2_mid)
+
+b_10_bp = c(b10bar0_bp, b10bar1_bp, b10bar2_bp)
+b_10_bpno = c(b10bar0_bpno, b10bar1_bpno, b10bar2_bpno)
+b_10_mid = c(b10bar0_mid, b10bar1_mid, b10bar2_mid)
+
+sdB_10_bp = c(sdBCbar1_bp, sdB10bar1_bp, sdB2bar1_bp)
+sdB_10_bpno = c(sdBCbar1_bpno, sdB10bar1_bpno, sdB2bar1_bpno)
+sdB_10_mid = c(sdBCbar1_mid, sdB10bar1_mid, sdB2bar1_mid)
+
+b_2_bp = c(b2bar0_bp, b2bar1_bp, b2bar2_bp)
+b_2_bpno = c(b2bar0_bpno, b2bar1_bpno, b2bar2_bpno)
+b_2_mid = c(b2bar0_mid, b2bar1_mid, b2bar2_mid)
+
+sdB_2_bp = c(sdB2bar0_bp, sdB2bar1_bp, sdB2bar2_bp)
+sdB_2_bpno = c(sdB2bar0_bpno, sdB2bar1_bpno, sdB2bar2_bpno)
+sdB_2_mid = c(sdB2bar0_mid, sdB2bar1_mid, sdB2bar2_mid)
+
+#Climate BIC
+x=c(0:2)
+plot(x, b_C_bp, xlab="BAR Number", ylab="Average BIC", main="Average BIC for Climate Data", col="red", ylim=c(-600,0),  cex = 1.5, pch=16)
+points(x, b_C_bp+sdB_C_bp*2, col="red", cex = 1, pch=16)
+points(x, b_C_bp-sdB_C_bp*2, col="red", cex = 1, pch=16)
+for(i in 0:10) {
+  lines(c(i,i),c(b_C_bp[i+1]-sdB_C_bp[i+1]*2,b_C_bp[i+1]+sdB_C_bp[i+1]*2), col="red",lty=2)
+}
+points(x, b_C_bpno, col="purple", cex = 1.5, pch=16)
+points(x, b_C_bpno+sdB_C_bpno*2, col="purple", cex = 1, pch=16)
+points(x, b_C_bpno-sdB_C_bpno*2, col="purple", cex = 1, pch=16)
+for(i in 0:10) {
+  lines(c(i,i),c(b_C_bpno[i+1]-sdB_C_bpno[i+1]*2,b_C_bpno[i+1]+sdB_C_bpno[i+1]*2), col="purple",lty=2)
+}
+
+points(x, b_C_mid, col="blue", cex = 1.5, pch=16)
+points(x, b_C_mid+sdB_C_mid*2, col="blue", cex = 1, pch=16)
+points(x, b_C_mid-sdB_C_mid*2, col="blue", cex = 1, pch=16)
+for(i in 0:10) {
+  lines(c(i,i),c(b_C_mid[i+1]-sdB_C_mid[i+1]*2,b_C_mid[i+1]+sdB_C_mid[i+1]*2), col="blue",lty=2)
+}
+
+legend(1, -50, legend=c("Bai-Perron Constrains","Bai-Perron No Constrains","Middle"),
+       col=c("red" ,"purple", "blue"), lty=1:1, cex=.5)
+
+
+#data10 BIC
+x=c(0:2)
+plot(x, b_10_bp, xlab="BAR Number", ylab="Average BIC", main="Average BIC for Data 10", col="red", ylim=c(200,600),  cex = 1.5, pch=16)
+points(x, b_10_bp+sdB_10_bp*2, col="red", cex = 1, pch=16)
+points(x, b_10_bp-sdB_10_bp*2, col="red", cex = 1, pch=16)
+for(i in 0:10) {
+  lines(c(i,i),c(b_10_bp[i+1]-sdB_10_bp[i+1]*2,b_10_bp[i+1]+sdB_10_bp[i+1]*2), col="red",lty=2)
+}
+points(x, b_10_bpno, col="purple", cex = 1.5, pch=16)
+points(x, b_10_bpno+sdB_10_bpno*2, col="purple", cex = 1, pch=16)
+points(x, b_10_bpno-sdB_10_bpno*2, col="purple", cex = 1, pch=16)
+for(i in 0:10) {
+  lines(c(i,i),c(b_10_bpno[i+1]-sdB_10_bpno[i+1]*2,b_10_bpno[i+1]+sdB_10_bpno[i+1]*2), col="purple",lty=2)
+}
+
+points(x, b_10_mid, col="blue", cex = 1.5, pch=16)
+points(x, b_10_mid+sdB_10_mid*2, col="blue", cex = 1, pch=16)
+points(x, b_10_mid-sdB_10_mid*2, col="blue", cex = 1, pch=16)
+for(i in 0:10) {
+  lines(c(i,i),c(b_10_mid[i+1]-sdB_10_mid[i+1]*2,b_10_mid[i+1]+sdB_10_mid[i+1]*2), col="blue",lty=2)
+}
+
+legend(1, 300, legend=c("Bai-Perron Constrains","Bai-Perron No Constrains","Middle"),
+       col=c("red" ,"purple", "blue"), lty=1:1, cex=.5)
+
+
+#Data 2
+x=c(0:2)
+plot(x, b_2_bp, xlab="BAR Number", ylab="Average BIC", main="Average BIC for Data2", col="red", ylim=c(200,400),  cex = 1.5, pch=16)
+points(x, b_2_bp+sdB_2_bp*2, col="red", cex = 1, pch=16)
+points(x, b_2_bp-sdB_2_bp*2, col="red", cex = 1, pch=16)
+for(i in 0:10) {
+  lines(c(i,i),c(b_2_bp[i+1]-sdB_2_bp[i+1]*2,b_2_bp[i+1]+sdB_2_bp[i+1]*2), col="red",lty=2)
+}
+points(x, b_2_bpno, col="purple", cex = 1.5, pch=16)
+points(x, b_2_bpno+sdB_2_bpno*2, col="purple", cex = 1, pch=16)
+points(x, b_2_bpno-sdB_2_bpno*2, col="purple", cex = 1, pch=16)
+for(i in 0:10) {
+  lines(c(i,i),c(b_2_bpno[i+1]-sdB_2_bpno[i+1]*2,b_2_bpno[i+1]+sdB_2_bpno[i+1]*2), col="purple",lty=2)
+}
+
+points(x, b_2_mid, col="blue", cex = 1.5, pch=16)
+points(x, b_2_mid+sdB_2_mid*2, col="blue", cex = 1, pch=16)
+points(x, b_2_mid-sdB_2_mid*2, col="blue", cex = 1, pch=16)
+for(i in 0:10) {
+  lines(c(i,i),c(b_2_mid[i+1]-sdB_2_mid[i+1]*2,b_2_mid[i+1]+sdB_2_mid[i+1]*2), col="blue",lty=2)
+}
+
+legend(1, 250, legend=c("Bai-Perron Constrains","Bai-Perron No Constrains","Middle"),
+       col=c("red" ,"purple", "blue"), lty=1:1, cex=.5)
 
 
