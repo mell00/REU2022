f456a8692fe673960d7b0f121483680eada70191
sneitzel <40214706+sneitzel@users.noreply.github.com>
2018-07-10 13:58:59 -0400

    Update time.R

--- Long_runs/time.R
+++ Long_runs/time.R
@@ -7,8 +7,8 @@ bar0_data2_middle | bar1_data2_middle | bar2_data2_middle
 
 bar0_data10_bpno | bar1_data10_bpno | bar2_data10_bpno
 3.560775 hours | 2.34281 hours | 2.823501 hours
-bar0_data10_bpwith | bar2_data10_bpwith
-3.676524 hours | 2.292334 hours
+bar0_data10_bpwith | bar1_data10_bpno | bar2_data10_bpwith
+3.676524 hours | 2.394353 hours | 2.292334 hours
 bar0_data10_middle | bar2_data10_middle
 2.899285 hours | 3.13553 hours
 
