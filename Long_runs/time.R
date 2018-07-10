7920c6526dceb6b83a1034240be7ad4341a546e2
sneitzel <40214706+sneitzel@users.noreply.github.com>
2018-07-10 14:01:53 -0400

    Update time.R

--- Long_runs/time.R
+++ Long_runs/time.R
@@ -12,8 +12,8 @@
 #bar0_data10_middle | bar1_data10_middle | bar2_data10_middle
 #2.899285 hours | 2.82572 hours | 3.13553 hours
 
-#bar0_climate_bpno | bar2_climate_bpno
-#5.537377 hours | 4.452221 hours
+#bar0_climate_bpno | bar1_climate_bpno | bar2_climate_bpno
+#5.537377 hours | 3.766292 hours | 4.452221 hours
 #bar0_climate_bpwith | bar1_climate_bpwith | bar2_climate_bpwith
 #5.412965 hours | 3.820345 hours | 4.503763 hours
 #bar0_climate_middle | bar1_climate_middle | bar2_climate_middle
