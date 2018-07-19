1dcd0e077981ff51f1f04923788cdd014902b305
sneitzel <sarahbneitzel.wa@gmail.com>
2018-07-19 15:08:03 -0400

    Check.

--- test_Cases/pelicanData.R
+++ test_Cases/pelicanData.R
@@ -3,7 +3,9 @@ pelican<-read.csv("pacificBrownPelican.csv")
 pelican<-pelican[-which(pelican$NumberByPartyHours == 0),]
 
 plot(pelican$NumberByPartyHours~pelican$Count_y, col="brown", pch=19, main="Pacific Brown Pelican Population: 1939 to 2017", ylab="Year (since 1900", xlab="Individuals per Party Hour")
+lines(pelican$Count_y, pelican$NumberByPartyHours, col="brown")
 points(c(39:117), fitted(arima(pelican$NumberByPartyHours, order=c(3,0,0))), col="green3", pch=19)
+lines(c(39:117), fitted(arima(pelican$NumberByPartyHours, order=c(3,0,0))), col="green3")
 
 pelican_bkpts<-bai_perron(pelican$NumberByPartyHours, pelican$Count_yr, "ar", "order=3", 5, 0.1, 3)
 pelican_bkpts
@@ -23,6 +25,8 @@ hist(pelican_result$NumBkpts, xlim=c(1,6), breaks=c(1:6), ylim=c(0,3000), right=
 hist(pelican_finbkpts, breaks=78, xlim=c(1,78), ylim=c(0,3000), right=F, xlab="Years Since 1939", ylab="Number of Iterations (Out of 3000)", main="Breakpoint Locations for Brown Pelicans", col="brown")
 
 plot(pelican$NumberByPartyHours~pelican$Count_y, xlab="Year (since 1900)", ylab="Individuals per Party Hour", main="Pacific Brown Pelican Population: 1939 to 2017", col="brown", pch=19)
+
 fits_to_use = pelican_result$Fits[which(pelican_result$Breakpoints[,1] == 11 & pelican_result$NumBkpts == 1),]
 points(c(39:49),colMeans(fits_to_use)[1:11],col="green3", pch=19)
+
 points(c(50:117),colMeans(fits_to_use)[12:79],col="blue", pch=19)
