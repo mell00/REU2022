eeb3e9065b2f494f870fb3d5f58f405bdf546331
sneitzel <sarahbneitzel.wa@gmail.com>
2018-07-23 23:17:29 -0400

    Working on case study.

--- test_Cases/pelicanData.R
+++ test_Cases/pelicanData.R
@@ -7,8 +7,10 @@ cols1<-c('#006e82', '#8214a0', '#005ac8', '#00a0fa', '#fa78fa', '#14d2dc', '#aa0
 pelican$Count_yr = pelican$Count_yr + 1899
 plot(pelican$NumberByPartyHours~pelican$Count_yr, col="#aa0a3c", xaxp=c(1940, 2015, 5), pch=19, main="Pacific Brown Pelican Population: 1938 to 2016", xlab="Year", ylab="Individuals per Party Hour")
 lines(pelican$Count_yr, pelican$NumberByPartyHours, col="#aa0a3c")
-points(c(39:117), fitted(arima(pelican$NumberByPartyHours, order=c(3,0,0))), col="green3", pch=19)
-lines(c(39:117), fitted(arima(pelican$NumberByPartyHours, order=c(3,0,0))), col="green3")
+library(“forecast”)
+points(c(1938:2016), fitted(arima(pelican$NumberByPartyHours, order=c(3,0,0))), col="#8214a0", pch=15)
+lines(c(1938:2016), fitted(arima(pelican$NumberByPartyHours, order=c(3,0,0))), col="#8214a0", lty=3)
+AIC(arima(pelican$NumberByPartyHours, order=c(3,0,0)),k = log(length(pelican$NumberByPartyHours)))
 
 pelican_bkpts<-bai_perron.ar(pelican$NumberByPartyHours, pelican$Count_yr, order=3, max_breaks=1)
 pelican_bkpts
@@ -27,11 +29,11 @@ for(i in 1:ncol(pelican_result$Breakpoints)){
 pelican_finbkpts = pelican_finbkpts+1937
 
 hist(pelican_result$NumBkpts, breaks=c(0.5,1.5,2.5), xlim=c(0.5,2.5), ylim=c(0,10000), right=F, xlab="Number of Breaks", ylab="Number of Iterations (out of 10,000)", main="Distribution of Breakpoint Number", col="#aa0a3c")
-hist(pelican_finbkpts, breaks=seq(1938.5,2016.5,1), xlim=c(1940,2010), ylim=c(0,10000), right=F, xlab="Year", ylab="Number of Iterations (out of 10,000)", main="Distribution of Breakpoint Locations", col="#aa0a3c")
-
-plot(pelican$NumberByPartyHours~pelican$Count_y, xlab="Year ", ylab="Individuals per Party Hour", main="Pacific Brown Pelican Population: 1939 to 2017", col="brown", pch=19)
+hist(pelican_finbkpts, breaks=seq(1938.5,2016.5,1), xlim=c(1940,2015), xaxp=c(1940, 2015, 5), ylim=c(0,10000), right=F, xlab="Year", ylab="Number of Iterations (out of 10,000)", main="Distribution of Breakpoint Locations", col="#aa0a3c")
 
 fits_to_use = pelican_result$Fits[which(pelican_result$Breakpoints[,1] == 11 & pelican_result$NumBkpts == 1),]
-points(c(39:49),colMeans(fits_to_use)[1:11],col="green3", pch=19)
-
-points(c(50:117),colMeans(fits_to_use)[12:79],col="blue", pch=19)
+points(c(1938:1948),colMeans(fits_to_use)[1:11], col="#00a0fa", pch=17)
+lines(c(1938:1948),colMeans(fits_to_use)[1:11], col="#00a0fa", lty=2)
+points(c(1949:2016),colMeans(fits_to_use)[12:79], col="#0ab45a", pch=18)
+lines(c(1949:2016),colMeans(fits_to_use)[12:79], col="#0ab45a", lty=2)
+mean(pelican_result$BIC[which(pelican_result$Breakpoints[,1] == 11 & pelican_result$NumBkpts == 1),])
