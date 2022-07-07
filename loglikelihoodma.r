013edb796e1d82d7378ab455999967f128aefb2e
Madison Ell <mellmell2550@gmail.com>
2022-07-07 10:19:43 -0400

    New epsilon t and loglikelihood functions

--- loglikelihoodma.r
+++ loglikelihoodma.r
@@ -1,11 +1,20 @@
-MAvariance = function(sigma,theta){
+
+
+epsilon_t = function(ma, k_ends, data.ts){
+  model = suppressWarnings(arima(full_data[,2], order=c(0,0,ma)))
+  for (i in 1:ma){
+    epsilon_list[i] = data.ts[i]-mu-epsilon_list[i-1]
+  }
 }
-loglikelihood <- toeplitz(ARMAacf(ma = x, lag.max = n - 1) * (1 + theta^2))
-Qinv <- chol2inv(chol(Q))
-tmp <- drop(y %*% Qinv %*% cbind(y)) / n
-Sval <- tmp * (det(Q))^(1/n)
 
+logl <- function(sigma,alpha,beta,data.ts) {
+  for (i in 1:length(model))
+  -dnorm(model[-1],alpha+beta*data.ts[1:length(model)-1],sigma,log=TRUE)
+}
 
+theta_list = c(0)
+
+theta_new = mvrnorm(tail(theta_list,1),
 
 fitMetrics<-function(k_ends, full_data){
   
@@ -15,16 +24,16 @@ fitMetrics<-function(k_ends, full_data){
   coef_2 = 0 
   #get and sum log likelihood for regressions of all intervals
   if(length(k_ends) < 3 ){
-    model = suppressWarnings(FitAR(full_data[,2], p=ma))
+    model = suppressWarnings(arima(full_data[,2], order=c(0,0,ma)))
     SEE = sum(na.omit(model$res)^2)
     s2 = SEE/n
-    sum_loglik = (-1*n/2)*(log(2*pi)+log(s2)+1) #finding the log likeihoods on the full dataset 
+    sum_loglik = (-1*n/2)*(log(2*pi)+log(s2)+1) #finding the log likelihoods on the full dataset 
   }else{
     for(i in 2:length(k_ends)) {
       if(i == 2){
         min = k_ends[i-1]
         y_values = full_data[c(min:k_ends[i]),2] #getting the y values in the interval
-        model = suppressWarnings(FitAR(y_values, p=ar))
+        model = suppressWarnings(arima(y_values, c(0,0,ma)))
         sub_n = length(y_values)
         SEE = sum(na.omit(model$res)^2)
         s2 = SEE/sub_n
@@ -33,7 +42,7 @@ fitMetrics<-function(k_ends, full_data){
       }else if(i > 2){
         min = k_ends[i-1]
         y_values = full_data[c((min+1):k_ends[i]),2] #getting the y values in the interval
-        model = suppressWarnings(FitAR(y_values, p=ar))
+        model = suppressWarnings(arima(y_values, c(0,0,ma)))
         sub_n = length(y_values)
         SEE = sum(na.omit(model$res)^2)
         s2 = SEE/sub_n
