138e25960cc222d8aaa10d94b62c75d4f0a25a5a
Madison Ell <mellmell2550@gmail.com>
2022-07-08 10:07:27 -0400

    Updated theta list

--- loglikelihoodma.r
+++ loglikelihoodma.r
@@ -1,7 +1,7 @@
 
 
 library(MASS)
- ma = 1
+ ma = 10
  k_ends = 5
  mu = 1.5
  tao = abs(1)
@@ -13,13 +13,14 @@ library(MASS)
  data.ts = arima(full_data$Count_yr,order=c(0,0,ma))
  
    
-   epsilon_list = c(1,2,3,4,5,6,7)
+   epsilon_list = c(0,1)
    epsilon_t = function(ma, k_ends, data.ts,mu){
-       for (i in 1:ma){
-           epsilon_list[i] = data.ts[i]- mu -epsilon_list[i-1]
+       for (i in 2:ma){
+           epsilon_list[i] = data.ts$residuals[i]- mu - epsilon_list[i-1]
          }
          return(epsilon_list)
-
+   }
+   epsilon_t(ma,k_ends,data.ts,mu)
 logl <- function(sigma,alpha,beta,data.ts) {
   sum_loglik = 0
   for (i in 1:length(model)){
@@ -27,9 +28,9 @@ logl <- function(sigma,alpha,beta,data.ts) {
   }
 }
 
-theta_list = c(0)
+theta_list = data.ts$model$theta
 
-theta_new = mvrnorm(tail(theta_list,sigma_mtrx))
+theta_new = mvrnorm(n=length(theta_list),theta_list,sigma_mtrx)
 
 fitMetrics<-function(k_ends, full_data){
   
@@ -53,7 +54,7 @@ fitMetrics<-function(k_ends, full_data){
         SEE = sum(na.omit(model$res)^2)
         s2 = SEE/sub_n
         sub_loglik = (-1*sub_n/2)*(log(2*pi)+log(s2)+1)
-        sum_loglik = sum_loglik + sub_loglik #the logLik looks the log likelyhood (relates to both SSR and MLE)
+        sum_loglik = sum_loglik + sub_loglik #the logLik looks the log likelihood (relates to both SSR and MLE)
       }else if(i > 2){
         min = k_ends[i-1]
         y_values = full_data[c((min+1):k_ends[i]),2] #getting the y values in the interval
