513bf16896ef9ed597d9e9cc65c3f1271f71b40a
Madison Ell <mellmell2550@gmail.com>
2022-07-08 12:06:40 -0400

    More work on epsilon list, new theta list, and likelihood formula

--- loglikelihoodma.r
+++ loglikelihoodma.r
@@ -1,36 +1,66 @@
-
-
 library(MASS)
  ma = 10
  k_ends = 5
  mu = 1.5
  tao = abs(1)
- sigma = c(tao,0,0,tao)
- sigma_mtrx = matrix(sigma,nrow=2,ncol=2,byrow=TRUE)
+ sigma_mtrx = diag(tao,length(theta_list))
  
  setwd("/Users/mellm/github/REU2022/test_Cases")
  full_data = read.csv("pacificBrownPelican.csv")
  data.ts = arima(full_data$Count_yr,order=c(0,0,ma))
- 
+ min = k_ends[i-1]
+ y_values = full_data[c(min:k_ends[i]),2]
    
-   epsilon_list = c(0,1)
-   epsilon_t = function(ma, k_ends, data.ts,mu){
+  epsilon_list = c(0,1)
+  epsilon_t = function(ma, k_ends, data.ts,mu){
+     if (ma == 0){ 
+       
+     }
+     if (ma == 1){ 
+       y_values[ma] - mu
+     }
+     else{
        for (i in 2:ma){
-           epsilon_list[i] = data.ts$residuals[i]- mu - epsilon_list[i-1]
-         }
-         return(epsilon_list)
+         epsilon_list[i] = data.ts$residuals[i]- mu - alpha*epsilon_list[i-1]
+       }
+       return(epsilon_list)
+     }
    }
    epsilon_t(ma,k_ends,data.ts,mu)
-logl <- function(sigma,alpha,beta,data.ts) {
+alpha = 1 #between -1 and 1
+logl <- function(sigma,alpha,beta,data.ts) { #log likelihood calculation
   sum_loglik = 0
-  for (i in 1:length(model)){
-    sum_loglik = sum_loglik - dnorm(model[-1],alpha+beta*data.ts[1:length(model)-1],sigma,log=TRUE)
+  for (i in 1:length(data.ts)){
+    sum_loglik = sum_loglik - prod(dnorm(y_values[i],alpha+beta*data.ts[1:length(data.ts)-1],sigma,log=TRUE))
   }
+  return(sum_loglik)
 }
 
 theta_list = data.ts$model$theta
 
-theta_new = mvrnorm(n=length(theta_list),theta_list,sigma_mtrx)
+theta_new = mvrnorm(n=1,mu=theta_list,Sigma=sigma_mtrx,tol = 1e-6, empirical = FALSE,EISPACK = FALSE)
+
+r = min()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 fitMetrics<-function(k_ends, full_data){
   
