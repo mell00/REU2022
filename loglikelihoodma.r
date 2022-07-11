3fee4d766c64abfa0a4bed56a417aa01b983db82
Madison Ell <mellmell2550@gmail.com>
2022-07-11 12:08:53 -0400

    fix k_ends, create separate matrix for data, re-add theta_new

--- loglikelihoodma.r
+++ loglikelihoodma.r
@@ -1,39 +1,19 @@
 library(MASS)
  
  setwd("/Users/mellm/github/REU2022/test_Cases")
- full_data = read.csv("pacificBrownPelican.csv")
- data.ts = arima(full_data$Count_yr,order=c(0,0,ma))
+ full_data1 = read.csv("pacificBrownPelican.csv")
+ full_data = cbind(c(1:length(as.numeric(full_data1$NumberByPartyHours))), as.numeric(full_data1$Count_yr))
+ data.ts = arima(full_data1$Count_yr,order=c(0,0,ma))
  ma = 10
  n = length(full_data[,1]) #number of observations
- k = tail(full_data$NumberByPartyHours,-1)
- k_ends = suppressWarnings(as.numeric(c(min(full_data[,2]), na.omit(k), n))) #adding end points to k
- k_ends = k_ends[!is.na(k_ends)]
+ k = tail(full_data,-1) #omitting non-numeric 1st row
+ k_ends = suppressWarnings(c(min(full_data[,1]), na.omit(k), n)) #adding end points to k
+
  mu = 1.5
  tao = abs(1)
  theta_list = data.ts$model$theta
+ theta_new = mvrnorm(n=length(theta_list),theta_list,sigma_mtrx)
  sigma_mtrx = diag(tao,length(theta_list))
- 
- if(length(k_ends) < 3 ){
-   SEE = sum(na.omit(data.ts$residuals)^2)
-   s2 = SEE/n
- } else {
-   for(i in 2:length(k_ends)) {
-     if(i == 2){
-       min = k_ends[i-1]
-       y_values = full_data[c(min:k_ends[i]),2] #getting the y values in the interval
-       sub_n = length(y_values)
-       SEE = sum(na.omit(data.ts$residuals)^2)
-       s2 = SEE/sub_n
-     }
-     else if(i > 2){
-       min = k_ends[i-1]
-       y_values = full_data[c(min:k_ends[i]),2] #getting the y values in the interval
-       sub_n = length(y_values)
-       SEE = sum(na.omit(data.ts$residuals)^2)
-       s2 = SEE/sub_n
-     }
-   }
- }
    
   epsilon_list = c(0,1)
   epsilon_t = function(ma, k_ends, data.ts,mu){
@@ -55,11 +35,35 @@ alpha = 1 #between -1 and 1
 logl <- function(sigma,alpha,beta,data.ts) { #log likelihood calculation
   sum_loglik = 0
   for (i in 1:length(data.ts)){
-    sum_loglik = sum_loglik - prod(dnorm(y_values[i],alpha+beta*data.ts[1:length(data.ts)-1],s2,log=TRUE))
+    if(length(k_ends) < 3 ){
+      SEE = sum(na.omit(data.ts$residuals)^2)
+      s2 = SEE/n
+      sum_loglik = sum_loglik - prod(dnorm(y_values[i],alpha+beta*data.ts[1:length(data.ts)-1],sigma,log=TRUE))
+    } else {
+      for(i in 2:length(k_ends)) {
+        if(i == 2){
+          min = k_ends[i-1]
+          y_values = full_data[c(min:k_ends[i]),2] #getting the y values in the interval
+          sub_n = length(y_values)
+          SEE = sum(na.omit(data.ts$residuals)^2)
+          s2 = SEE/sub_n
+          sum_loglik = sum_loglik - prod(dnorm(y_values[i],alpha+beta*data.ts[1:length(data.ts)-1],sigma,log=TRUE))
+        }
+        else if(i > 2){
+          min = k_ends[i-1]
+          y_values = full_data[c(min:k_ends[i]),2] #getting the y values in the interval
+          sub_n = length(y_values)
+          SEE = sum(na.omit(data.ts$residuals)^2)
+          s2 = SEE/sub_n
+          sum_loglik = sum_loglik - prod(dnorm(y_values[i],alpha+beta*data.ts[1:length(data.ts)-1],sigma,log=TRUE))
+        }
+      }
+    }
   }
   return(sum_loglik)
 }
 
+logl(s2,alpha,beta,data.ts)
 theta_new = mvrnorm(n=1,mu=theta_list,Sigma=sigma_mtrx,tol = 1e-6, empirical = FALSE,EISPACK = FALSE)
 
 r = min()
@@ -91,7 +95,7 @@ fitMetrics<-function(k_ends, full_data){
   coef_1 = 0 
   coef_2 = 0 
   #get and sum log likelihood for regressions of all intervals
-  if(length(k_ends) < 3 ){
+  if(length(k_ends) < 3){
     model = suppressWarnings(arima(full_data[,2], order=c(0,0,ma)))
     SEE = sum(na.omit(model$res)^2)
     s2 = SEE/n
