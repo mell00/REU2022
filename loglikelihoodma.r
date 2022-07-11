d07bc3dc608f453211df9616ea58393a2251bad0
Madison Ell <mellmell2550@gmail.com>
2022-07-11 13:38:28 -0400

    finished first draft of ma logliklihood, needs to be tested

--- loglikelihoodma.r
+++ loglikelihoodma.r
@@ -31,14 +31,22 @@ library(MASS)
      }
    }
    epsilon_t(ma,k_ends,data.ts,mu)
+   
 alpha = 1 #between -1 and 1
+sum_loglik = function(){
+  for (i in 1:length(full_data)-1){
+    sum_loglik = prod(dnorm(y_values[i],alpha+beta*full_data[i],sigma,log=TRUE))
+  }
+  return(sum_loglik)
+}
+
 logl <- function(sigma,alpha,beta,data.ts) { #log likelihood calculation
   sum_loglik = 0
   for (i in 1:length(data.ts)){
     if(length(k_ends) < 3 ){
       SEE = sum(na.omit(data.ts$residuals)^2)
       s2 = SEE/n
-      sum_loglik = sum_loglik - prod(dnorm(y_values[i],alpha+beta*data.ts[1:length(data.ts)-1],sigma,log=TRUE))
+      return(sum_loglik())
     } else {
       for(i in 2:length(k_ends)) {
         if(i == 2){
@@ -47,7 +55,7 @@ logl <- function(sigma,alpha,beta,data.ts) { #log likelihood calculation
           sub_n = length(y_values)
           SEE = sum(na.omit(data.ts$residuals)^2)
           s2 = SEE/sub_n
-          sum_loglik = sum_loglik - prod(dnorm(y_values[i],alpha+beta*data.ts[1:length(data.ts)-1],sigma,log=TRUE))
+          return(sum_loglik())
         }
         else if(i > 2){
           min = k_ends[i-1]
@@ -55,73 +63,14 @@ logl <- function(sigma,alpha,beta,data.ts) { #log likelihood calculation
           sub_n = length(y_values)
           SEE = sum(na.omit(data.ts$residuals)^2)
           s2 = SEE/sub_n
-          sum_loglik = sum_loglik - prod(dnorm(y_values[i],alpha+beta*data.ts[1:length(data.ts)-1],sigma,log=TRUE))
+          return(sum_loglik())
         }
       }
     }
   }
-  return(sum_loglik)
 }
 
 logl(s2,alpha,beta,data.ts)
 theta_new = mvrnorm(n=1,mu=theta_list,Sigma=sigma_mtrx,tol = 1e-6, empirical = FALSE,EISPACK = FALSE)
 
-r = min()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-fitMetrics<-function(k_ends, full_data){
-  
-  #create sum objects
-  sum_loglik = 0
-  coef_1 = 0 
-  coef_2 = 0 
-  #get and sum log likelihood for regressions of all intervals
-  if(length(k_ends) < 3){
-    model = suppressWarnings(arima(full_data[,2], order=c(0,0,ma)))
-    SEE = sum(na.omit(model$res)^2)
-    s2 = SEE/n
-    sum_loglik = sum_loglik - prod(dnorm(y_values[i],alpha+beta*data.ts[1:length(data.ts)-1],sigma,log=TRUE)) #finding the log likelihoods on the full dataset 
-  }else{
-    for(i in 2:length(k_ends)) {
-      if(i == 2){
-        min = k_ends[i-1]
-        y_values = full_data[c(min:k_ends[i]),2] #getting the y values in the interval
-        model = suppressWarnings(arima(y_values, c(0,0,ma)))
-        sub_n = length(y_values)
-        SEE = sum(na.omit(model$res)^2)
-        s2 = SEE/sub_n
-        sub_loglik = (-1*sub_n/2)*(log(2*pi)+log(s2)+1)
-        sum_loglik = sum_loglik + sub_loglik #the logLik looks the log likelihood (relates to both SSR and MLE)
-      }else if(i > 2){
-        min = k_ends[i-1]
-        y_values = full_data[c((min+1):k_ends[i]),2] #getting the y values in the interval
-        model = suppressWarnings(arima(y_values, c(0,0,ma)))
-        sub_n = length(y_values)
-        SEE = sum(na.omit(model$res)^2)
-        s2 = SEE/sub_n
-        sub_loglik = (-1*sub_n/2)*(log(2*pi)+log(s2)+1)
-        sum_loglik = sum_loglik + sub_loglik #the logLik looks the log likelihood (relates to both SSR and MLE)
-      }
-    }
-  }
-  return(sum_loglik)
-}
+r = min((logl(theta_new))/(logl(theta_list)),1)
