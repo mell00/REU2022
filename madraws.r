5cefcfeae655cc5ea2f67632e9de99fcba202903
Madison Ell <mellmell2550@gmail.com>
2022-07-14 11:11:12 -0400

    big_B needs work

--- madraws.r
+++ madraws.r
@@ -1,26 +1,82 @@
 setwd("\\Users\\mellm\\github\\REU2022")
 source("loglikelihoodma.r")
 
-big_theta = NA
-l = NA
-big_B = NA
+z = runif(1,0,1) #1 value between 0 and 1
+hat_theta = theta_new #TEST
+t = seq(1:10) #sequence of integers
+q = 5 # a positive integer
+Z_t = abs(rnorm(t)) #iid sequence of non-negative random variables
+
+#x_sum
+i = 1:q; x_sum = sum(hat_theta[i]*Z_t[i-1])
 
 #MA equation (work in progress)
-x_t = big_theta(big_B)
+x_t = function (big_theta,big_B,Z_t){
+  for (t in q){
+    x_t[t] = big_theta[t]*big_B[t]*Z_t[t]
+  }
+  return(x_t)
+}
+
+big_theta = function(z_0){
+  i = 0:q; big_theta = sum(hat_theta[i]*z_0^i)
+  return(big_theta) #assumed to be non-zero
+}
+
+
+l = NA
+
+big_B = function(process){ #NEEDS WORK
+  for (t in length(process)){
+    big_B[t] = (process[t])/(process[t+1])
+  }
+  return(big_B)
+}
+
+big_B(Z_t)
 
 #theta sum
-i = 0:q; sum(hat_theta[i])
+i = 0:q; theta_sum = sum(hat_theta[i])
 
 #D_n sum
-k = 0:2l; (sum(big_I - big_theta(big_B))^k)x_t
+d_sum= 0:(2*l); (sum(big_I - big_theta(big_B))^k)x_t
 
 #argmax of D_n
 hat_theta = list(hat_theta, theta)
 outputs = sapply(hat_theta, D_n)
-best.input = hat_theta[which.max(outputs)]
+bestD_n = hat_theta[which.max(outputs)]
 
 #D_n
 
+#setting up priors for theta draws (define what b_0 and B_0 are)
+if(fit_storage == TRUE){
+  alt_arima<-function(full_data, ar){
+    tryCatch(arima(full_data[,2], method="ML", order=c(ar,0,0)), error = function(e) arima(full_data[,2], method="CSS", order=c(ar,0,0)))
+  }
+  model = suppressWarnings(alt_arima(full_data, ar))
+  informationless = matrix(0, ncol=(ar+1), nrow=(ar+1))
+  diag(informationless) = rep(1000, (ar+1))
+  alt_solve<-function(model_coef){
+    tryCatch(solve(model_coef), error = function(e) informationless)
+  }
+  fisher = suppressWarnings(alt_solve(model$var.coef)) #amount of data contained in 1 data point
+  smiley = n * fisher #empirical Bayes (using data to set priors) #as n goes to inf, variance becomes unbiased
+  
+  coef_list = model$coef[[length(model$coef)]]
+  
+  for(a in 1:(length(model$coef)-1)){
+    
+    coef_list = c(coef_list, model$coef[[a]], recursive=T) #pulls each coefficient from model
+    
+  }
+  
+  b_0 = matrix(coef_list,(ar+1),1) #matrix of beta means for posterior draw
+  B_0 = smiley #variance-covariance matrix for posterior draw
+  
+  #beta and sigma draw
+  post_theta_list = data.frame(Empty=rep(NA,(ar+1)))
+  post_sigma_list = data.frame(Empty=NA)
+}
 
 #setting up posterior
 
@@ -63,11 +119,11 @@ if(fit_storage == TRUE){
     bar_v = c(bar_v, v)
     bar_theta = c(bar_theta, theta)
     
-    #SIGMA:
-    v0 = (max(k_ends))/2 + 2
-    d0 = 0 + .5 * t(y_j - x_j %*% post_theta ) %*% (y_j - x_j %*% post_theta)
+    #SIGMA: #CHECK LATER
+    alpha_0 = (max(k_ends))/2 + 2
+    beta_0 = sum(epsilon_list^2)
     
-    sigma = rgamma(1, v0, rate = d0)
+    sigma = rgamma(1, alpha_0, rate = beta_0)
     post_sigma = 1 / sigma
     
     current_post_thetas = cbind(current_post_thetas, post_theta)
