060f6e17883416194c28aa09eaf7175997508022
Madison Ell <mellmell2550@gmail.com>
2022-07-26 14:35:12 -0400

    I think this is working, please test

--- test_Cases/suicideRatesData.r
+++ test_Cases/suicideRatesData.r
@@ -1,70 +1,61 @@
+library(devtools)
+#install_version("FitAR", version = "1.94", repos = "http://cran.us.r-project.org")
+#library(FitAR)
+library(forecast)
+library(strucchange)
+library(stats)
+library(MASS)
 par(mar=c(1,1,1,1))
 setwd("/Users/mellm/github/REU2022/test_Cases")
 suicide<-read.csv("suicideRatesData.csv")
 attach(suicide)
 
-cols1<-c('#006e82', '#8214a0', '#005ac8', '#00a0fa', '#fa78fa', '#14d2dc', '#aa0a3c', '#fa7850', '#0ab45a', '#f0f032', '#a0fa82', '#fae6be')
 
 library("forecast")
 setwd("/Users/mellm/github/REU2022/")
 source("ar_bai_perron.r")
 source("data_for_trials.r")
 source("Variations_of_bars/fixedbaar1.r")
-single_model <- arima(suicide$X15.24.years, order=c(3,0,0))
-single_fitted <- fitted(single_model)
-single_BIC <- BIC(single_model)
 
-suicide_bkpts<-bai_perron.ar(suicide$Year, suicide$X15.24.years, order=3, max_breaks=1)
-suicide_bkpts<-all_breakpoints(suicide$X15.24.years~suicide$Year)
 
-suicide_result<-baar(suicide_bkpts$Breakpoints, suicide$Year, suicide$X15.24.years, 10000, 1500, jump=0.25, ar=3)
-#suicide_result<-bama(suicide_bkpts$Breakpoints, suicide$Year, suicide$X15.24.years, 10000, 1500, jump=0.25)
-saveRDS(suicide_result, file="casestudy_data3.RData")
-
-#suicide_result<-readRDS("casestudy_data4.RData")
-suicide_result<-readRDS("test_Cases/casestudy_data3.RData")
-length(which(suicide_result$Breakpoints[,1] == 11)) +
-  length(which(suicide_result$Breakpoints[,1] == 12)) +
-  length(which(suicide_result$Breakpoints[,1] == 13)) +
-  length(which(suicide_result$Breakpoints[,1] == 14))
-length(which(suicide_result$Breakpoints[,2] == 70)) +
-  length(which(suicide_result$Breakpoints[,2] == 71)) +
-  length(which(suicide_result$Breakpoints[,2] == 72))
-
-suicide_finbkpts<-NULL
-
-for(i in 1:ncol(suicide_result$Breakpoints)){
-  
-  suicide_finbkpts<-c(suicide_finbkpts, na.omit(suicide_result$Breakpoints[,i]), recursive=T)
-  
+iterations=1000
+runs=10
+L=matrix(NA,nrow=iterations,ncol=runs)
+A=NA
+M=NA
+B=NA
+for(i in 1:runs){
+  current_data = suicide
+  break_p = breakpoints(current_data[,3] ~ current_data[,1], breaks = 5, h = 0.1) 
+  starting_breakpoints = break_p$breakpoints
+  test1=baar(starting_breakpoints,1:37,current_data[,3],iterations)
+  print(i)
+  L[,i]=test1$NumBkpts
+  M[i]=length(starting_breakpoints[!is.na(starting_breakpoints)])
+  A=c(A,(unlist(test1$Breakpoints))) #Baar location of breakpoints
+  B=c(B,(starting_breakpoints)) #B-P
 }
+A2=A[!is.na(A)]
+B2=B[!is.na(B)]
+mean(L)
+sd(L)
+mean(M)
+sd(M)
+plot(apply(L,1,mean))
+hist(A2, breaks=100, main="BAAR Breakpoints", ylab="Number of iterations (out of interations*runs)", xlab="Time")#50= 5000(iterations*runs)
+abline(v=17, col="red",lwd = 3)# change v to match breakpoints
+hist(B2, breaks=100, main="Bai-Perron Breakpoints", xlab="time") #(frenquency/runs)
 
-suicide_finbkpts = suicide_finbkpts #NEEDS TO BE CHANGED
+#percentage graphs "saved graphs"
+#Step1: Change the axis to match percents
+#Step2: Save onto REU folder shared as in the following order BAAR(orBP)_beta1value_beta2value_sdvalue_@breakpointlocation
+#example BAAR7_3_1_@45
+#example BP7_3_1_@45
+hist(A2, breaks=100, main="BAAR Breakpoints", ylab="Percentage of time location chosen", xlab="Time",yaxt="n")
+axis(2, at=c(0,1000,2000,3000,4000),labels=c(0,1000/100000,2000/100000,3000/100000,4000/100000)) #100,000=(iterations*runs)
+abline(v=45, col="red",lwd = 3)# change v to match breakpoints
+hist(B2, breaks=100, main="Bai-Perron Breakpoints", ylab= "Percentage of time location chosen ", xlab="Time",yaxt="n")
+axis(2, at=c(0,3.5,7,10.5,14),labels=c(0,3.5/100,7/100,10.5/100,14/100))#(frenquency/runs)
+abline(v=45, col="red",lwd = 3)# change v to match breakpoints
 
-hist(suicide_result$NumBkpts, breaks=c(0.5,1.5,2.5), xlim=c(0.5,2.5), ylim=c(0,10000), right=F, xlab="Number of Breaks", ylab="Number of Iterations (out of 10,000)", main="Distribution of Breakpoint Number", col="#aa0a3c")
-hist(suicide_finbkpts, breaks=seq(1977.5,2016.5,1), xlim=c(1979,2015), xaxp=c(1979, 2015, 5), ylim=c(0,10000), right=F, xlab="Year", ylab="Number of Iterations (out of 10,000)", main="Distribution of Breakpoint Locations", col="#aa0a3c")
-
-beta_to_use = suicide_result$Beta[which(suicide_result$Breakpoints[,1] == 11 & suicide_result$NumBkpts == 1)]
-sigma_to_use = suicide_result$Sigma[which(suicide_result$Breakpoints[,1] == 11 & suicide_result$NumBkpts == 1)]
-fits_to_use = suicide_result$Fits[which(suicide_result$Breakpoints[,1] == 11 & suicide_result$NumBkpts == 1),]
-
-lower = apply(fits_to_use, 2, quantile, probs = 0.025, na.rm = T)
-upper = apply(fits_to_use, 2, quantile, probs = 0.975, na.rm = T)
-
-par(mfrow=c(1,2))
-plot(suicide$Year, suicide$X15.24.years, ylim=c(-1,2.5), col="#aa0a3c", xaxp=c(1979, 2015, 5), pch=19, main="A. Recorded Suicides in American 15-24 year olds between 1979 and 2015: Single AR(3) Fit", xlab="Year", ylab="Count")
-lines(suicide$Year, suicide$X15.24.years, col="#aa0a3c")
-points(c(1979:2015), single_fitted, col="#8214a0", pch=15)
-lines(c(1979:2015), single_fitted, col="#8214a0", lty=1)
-
-plot(suicide$X15.24.years~suicide$Year, ylim=c(-1,2.5), col="#aa0a3c", xaxp=c(1940, 2015, 5), pch=19, main="B. Recorded Suicides in American 15-24 year olds between 1979 and 2015: Fit from BAAR", xlab="Year", ylab="Count")
-lines(suicide$Year, suicide$X15.24.years, col="#aa0a3c")
-points(c(1938:1948),colMeans(fits_to_use)[1:11], col="#00a0fa", pch=17)
-lines(c(1938:1948),colMeans(fits_to_use)[1:11], col="#00a0fa", lty=1)
-points(c(1949:2016),colMeans(fits_to_use)[12:79], col="#0ab45a", pch=18)
-lines(c(1949:2016),colMeans(fits_to_use)[12:79], col="#0ab45a", lty=1)
-lines(c(1938:1948),lower[1:11], col="#00a0fa", lty=3)
-lines(c(1949:2016),lower[12:79], col="#0ab45a", lty=3)
-lines(c(1938:1948),upper[1:11], col="#00a0fa", lty=3)
-lines(c(1949:2016),upper[12:79], col="#0ab45a", lty=3)
 
